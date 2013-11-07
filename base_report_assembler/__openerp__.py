bfb191e0ed9b1ac09320674be9b7b1fc2df6da39
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-11-07 10:59:40 +0100

    [ADD] module base_report_assembler

---
+++ base_report_assembler/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name' : 'Base Report Assembler',
+ 'version' : '1.0',
+ 'category': 'Report',
+ 'description': """Defines a new type of report which is an assemblage of multiple other reports""",
+ 'author' : 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'website': 'http://www.camptocamp.com/',
+ 'depends' : ['base'],
+ 'data': [],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'application': False,
+ }
