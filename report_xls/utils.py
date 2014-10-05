7480f51e99727a80e57fc4b4c7414a6b9f508e3c
luc-demeyer <luc.demeyer@noviat.com>
2014-10-05 18:28:25 +0200

    80 - report_xls port

--- report_xls/utils.py
+++ report_xls/utils.py
@@ -20,7 +20,32 @@
 #
 ##############################################################################
 
-from . import ir_report
-from . import report_xls
+
+def _render(code):
+    return compile(code, '<string>', 'eval')
+
+
+def rowcol_to_cell(row, col, row_abs=False, col_abs=False):
+    # Code based upon utils from xlwt distribution
+    """
+    Convert numeric row/col notation to an Excel cell
+    reference string in A1 notation.
+    """
+    d = col // 26
+    m = col % 26
+    chr1 = ""    # Most significant character in AA1
+    if row_abs:
+        row_abs = '$'
+    else:
+        row_abs = ''
+    if col_abs:
+        col_abs = '$'
+    else:
+        col_abs = ''
+    if d > 0:
+        chr1 = chr(ord('A') + d - 1)
+    chr2 = chr(ord('A') + m)
+    # Zero index to 1-index
+    return col_abs + chr1 + chr2 + row_abs + str(row + 1)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
