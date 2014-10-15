fb5b0c6a9c534e06e45ec9620ee87e9a1616c4eb
Anael Lorimier <anael.lorimier@xcg-consulting.fr>
2014-10-15 17:07:40 +0200

    1.2.5

--- __openerp__.py
+++ __openerp__.py
@@ -8,7 +8,7 @@ templates.
 The py3o.template package is required; install it with:
     pip install py3o.template
 ''',
-    'version': '1.2.4',
+    'version': '1.2.5',
     'category': 'Reporting',
     'author': 'XCG Consulting',
     'website': 'http://www.openerp-experts.com/',
