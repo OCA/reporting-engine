baacc1ab5350a9469a8ec8e9f3b57166519008b8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-12-08 08:50:03 +0100

    Rename base_condition_template to base_comment_template

---
+++ base_comment_template/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Author: Nicolas Bessi
+#    Copyright 2013-2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{"name": "Base Comments Templates",
+ "summary": "Comments templates on documents",
+ "version": "8.0.1.0.0",
+ "depends": ["base"],
+ "author": "Camptocamp,Odoo Community Association (OCA)",
+ "data": ["comment_view.xml",
+          'security/ir.model.access.csv',
+          ],
+ "category": "Sale",
+ "installable": True,
+ "active": False, }
