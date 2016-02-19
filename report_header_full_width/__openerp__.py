2966efc718470077bca14c3ecdb0338b010d7ce8
Sergio Incaser <sergio@incaser.es>
2016-02-19 12:51:25 +0100

    [NEW][8.0] report_header_full_width: More header width for company address.

---
+++ report_header_full_width/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Report Header Full Width',
+    'summary': 'Full width for reports header',
+    'version': '8.0.1.0.0',
+    'author': 'Incaser Informatica S.L., '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'http://www.incaser.es',
+    'category': 'Tools',
+    'depends': [
+        'report',
+    ],
+    'data': [
+        'views/layouts.xml',
+    ],
+    'images': [],
+    'installable': True,
+}
