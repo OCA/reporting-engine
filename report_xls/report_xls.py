OCA/reporting-engine

commit 65223f4a163244bae77f8493919d194428fe741b

[FIX][report_xls] Protect import. (#64)
[FIX][report_xls] Protect import.

Even after merging 2bf93a1d49f7c79062ab7c237ceb794cdbdec983, import still breaks when trying to use module's stuff at class definition time when module is not imported.

I move xsl types to a failure-safe scope.

report_xls/report_xls.py
@@ -29,13 +29,29 @@ from types import CodeType
 from openerp.report.report_sxw import report_sxw
 from openerp import pooler
 import logging
+
 _logger = logging.getLogger(__name__)
+
+xls_types_default = {
+    'bool': False,
+    'date': None,
+    'text': '',
+    'number': 0,
+}
 
 try:
     import xlwt
     from xlwt.Style import default_style
-except ImportError:
+
+    xls_types = {
+        'bool': xlwt.Row.set_cell_boolean,
+        'date': xlwt.Row.set_cell_date,
+        'text': xlwt.Row.set_cell_text,
+        'number': xlwt.Row.set_cell_number,
+    }
+except ImportError:  # pragma: no cover
     _logger.debug("Cannot import xlwt. This module will not be functional.")
+    xls_types = xls_types_default
 
 
 class AttrDict(dict):
@@ -45,22 +61,7 @@ class AttrDict(dict):
 
 
 class report_xls(report_sxw):
-
-    xls_types = {
-        'bool': xlwt.Row.set_cell_boolean,
-        'date': xlwt.Row.set_cell_date,
-        'text': xlwt.Row.set_cell_text,
-        'number': xlwt.Row.set_cell_number,
-    }
-    xls_types_default = {
-        'bool': False,
-        'date': None,
-        'text': '',
-        'number': 0,
-    }
-
     # TO DO: move parameters infra to configurable data
-
     # header/footer
     hf_params = {
         'font_size': 8,

commit 64d940c735464364821a9d8b99d9b5831a28d26d

[FIX][report_xls] Fix regression introduced in 65223f4.
Variables are now at module level because they have to be mocked when there is
an `ImportError`, so we got an `AttributeError` when trying to use them.

report_xls/report_xls.py
@@ -208,7 +208,7 @@ class report_xls(report_sxw):
                         c.append({'formula': s[5]})
                     else:
                         c.append({
-                            'write_cell_func': report_xls.xls_types[c[3]]})
+                            'write_cell_func': xls_types[c[3]]})
                     # Set custom cell style
                     if s_len > 6 and s[6] is not None:
                         c.append(s[6])
@@ -237,7 +237,7 @@ class report_xls(report_sxw):
             style = spec[6] and spec[6] or row_style
             if not data:
                 # if no data, use default values
-                data = report_xls.xls_types_default[spec[3]]
+                data = xls_types_default[spec[3]]
             if size != 1:
                 if formula:
                     ws.write_merge(