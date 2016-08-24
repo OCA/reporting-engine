cb8d66fac665ec686bb03db189780e7839862c9d
leNeo <fsiffert@gmail.com>
2016-08-24 18:53:04 +0200

    [MIG] base_comment_template to 9.0

--- base_comment_template/__openerp__.py
+++ base_comment_template/__openerp__.py
@@ -1,31 +1,14 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Author: Nicolas Bessi
-#    Copyright 2013-2014 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-{"name": "Base Comments Templates",
- "summary": "Comments templates on documents",
- "version": "8.0.1.0.0",
- "depends": ["base"],
- "author": "Camptocamp,Odoo Community Association (OCA)",
- "data": ["comment_view.xml",
+# © 2013-2014 Nicolas Bessi (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{'name': 'Base Comments Templates',
+ 'summary': 'Comments templates on documents',
+ 'version': '9.0.1.0.0',
+ 'license': 'AGPL-3',
+ 'depends': ['base', ],
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'data': ['views/comment.xml',
           'security/ir.model.access.csv',
           ],
- "category": "Sale",
- "installable": True,
- "active": False, }
+ 'category': 'Sale',
+ 'installable': True, }
