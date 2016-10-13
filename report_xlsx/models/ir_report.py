b5733e1951cda9aa4e85c56e13fb0efb7d3a6d6f
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-13 08:24:08 +0200

    [MIG] Migration of report_xlsx to 10.0 (#76)

--- report_xlsx/models/ir_report.py
+++ report_xlsx/models/ir_report.py
@@ -2,7 +2,7 @@
 # Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class IrActionsReportXml(models.Model):
