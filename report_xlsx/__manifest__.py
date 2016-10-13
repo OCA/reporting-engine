00551a7b7577583cb776e48d4c055d224c3ea7be
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-13 08:24:08 +0200

    [MIG] Migration of report_xlsx to 10.0 (#76)
    [MIG] Migration of report_xlsx to 10.0

--- report_xlsx/__manifest__.py
+++ report_xlsx/__manifest__.py
@@ -10,13 +10,11 @@
               'Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Reporting',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
-    'external_dependencies': {'python': ['xlsxwriter'],
-    'installable': False,
-},
+    'external_dependencies': {'python': ['xlsxwriter']},
     'depends': [
         'base',
     ],
-    'installable': False,
+    'installable': True,
 }
