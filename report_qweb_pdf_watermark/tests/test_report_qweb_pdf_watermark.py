a758aabc871359f835f7f636908a7a0864142e09
Holger Brunn <hbrunn@therp.nl>
2017-01-09 21:48:17 +0100

    report_qweb_pdf_watermark (#86)

---
+++ report_qweb_pdf_watermark/tests/test_report_qweb_pdf_watermark.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp.tests.common import TransactionCase
+
+
+class TestReportQwebPdfWatermark(TransactionCase):
+    def test_report_qweb_pdf_watermark(self):
+        # with our image, we have three
+        self._test_report_images(3)
+
+        self.env.ref('report_qweb_pdf_watermark.demo_report').write({
+            'pdf_watermark_expression': False,
+        })
+        # without, we have two
+        self._test_report_images(2)
+
+        self.env.ref('report_qweb_pdf_watermark.demo_report').write({
+            'pdf_watermark': self.env.user.company_id.logo,
+        })
+        # and now we should have three again
+        self._test_report_images(3)
+
+    def _test_report_images(self, number):
+        pdf = self.registry['report'].get_pdf(
+            self.cr,
+            self.uid,
+            self.env['res.users'].search([]).ids,
+            'report_qweb_pdf_watermark.demo_report_view',
+            context={}
+        )
+        self.assertEqual(pdf.count('/Subtype /Image'), number)
