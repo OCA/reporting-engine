bcfceb2cb26d80d4eda5f0b3752517d3dc5788a8
mreficent <miquel.raich@eficent.com>
2017-01-20 13:08:16 +0100

    [MIG] Migrate report_xls to v9.0

--- report_xls/__openerp__.py
+++ report_xls/__openerp__.py
@@ -1,27 +1,9 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2014 Noviat nv/sa (www.noviat.com). All rights reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 Noviat nv/sa (<http://www.noviat.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Excel report engine',
-    'version': '8.0.0.6.1',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Noviat,Odoo Community Association (OCA)",
     'website': 'http://www.noviat.com',
@@ -77,6 +59,5 @@ Excel reports in odoo.
     'depends': ['base'],
     'external_dependencies': {'python': ['xlwt']},
     'active': False,
-    'installable': False,
+    'installable': True,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- report_xls/utils.py
+++ report_xls/utils.py
@@ -1,24 +1,6 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2014 Noviat nv/sa (www.noviat.com). All rights reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 Noviat nv/sa (<http://www.noviat.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 def _render(code):
@@ -48,4 +30,3 @@ def rowcol_to_cell(row, col, row_abs=False, col_abs=False):
     # Zero index to 1-index
     return col_abs + chr1 + chr2 + row_abs + str(row + 1)
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
