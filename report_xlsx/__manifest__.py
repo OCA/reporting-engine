6f97e2c3ed93e244050dc743c3d3eae08b44dcbc
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-02-22 13:58:38 +0100

    [10.0] report_xlsx: Allow to define options for workbook creation (#98)
    * Allow to define options for workbook creation

--- report_xlsx/__manifest__.py
+++ report_xlsx/__manifest__.py
@@ -10,7 +10,7 @@
               'Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Reporting',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'license': 'AGPL-3',
     'external_dependencies': {'python': ['xlsxwriter']},
     'depends': [
