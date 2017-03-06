59f7360b2310db588588bd6651ce11cbce11c3f9
Andrea <a.stirpe@onestein.nl>
2017-03-06 09:44:00 +0100

    Initial commit bi_view_editor V9

---
+++ bi_view_editor/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import bve_view
+from . import ir_model
