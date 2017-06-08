df96999c87c77b6595476416f0abe9dc93fc10bf
Holger Brunn <hbrunn@therp.nl>
2017-06-08 17:01:05 +0200

    [IMP] use PyPDF2 if possible

--- report_qweb_pdf_watermark/__manifest__.py
+++ report_qweb_pdf_watermark/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Pdf watermark",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": "Therp BV, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
