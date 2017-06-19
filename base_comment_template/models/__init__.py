cef0aac0b86b842031d5cf527aaba47c7bb0101d
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2017-06-19 13:34:20 +0200

    [MIG] base_comment_template, invoice_comment_template: Migration to 10.0
    Port modules base_comment_template and invoice_comment_template to V10

--- base_comment_template/models/__init__.py
+++ base_comment_template/models/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2014 Guewen Baconnier (Camptocamp SA)
+# Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import comment
