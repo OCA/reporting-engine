57aca5f55fde2c1227628cce42a2aab1b500cba9
nicomacr <nmr@adhoc.com.ar>
2017-07-11 14:03:55 -0300

    bi_sql_editor

--- bi_sql_editor/__openerp__.py
+++ bi_sql_editor/__openerp__.py
@@ -6,7 +6,7 @@
 {
     'name': 'BI SQL Editor',
     'summary': "BI Views builder, based on Materialized or Normal SQL Views",
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Reporting',
     'author': 'GRAP,Odoo Community Association (OCA)',
