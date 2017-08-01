9698416b18ab52a47b2c2e41cd27ef05d1534a1c
Ruud Verbeek <rverbeek@therp.nl>
2017-08-01 10:55:42 +0200

    report_render_chrome base

---
+++ report_render_chrome/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Render reports using Chrome headless mode",
+    "version": "9.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "",
+    "summary": "",
+    "depends": [
+    ],
+    "data": [
+    ],
+    "qweb": [
+    ],
+    "test": [
+    ],
+    "images": [
+    ],
+    "pre_init_hook": False,
+    "post_init_hook": False,
+    "uninstall_hook": False,
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
