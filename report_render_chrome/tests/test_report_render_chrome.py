8ccafcc3d9d5bae74f43315c58e7423ca2bd9181
Holger Brunn <hbrunn@therp.nl>
2017-08-29 21:20:56 +0200

    [ADD] disable sandbox for tests

--- report_render_chrome/tests/test_report_render_chrome.py
+++ report_render_chrome/tests/test_report_render_chrome.py
@@ -9,6 +9,10 @@ from openerp.tests.common import HttpCase
 class TestReportRenderChrome(HttpCase):
     def test_report_render_chrome(self):
         self.authenticate('admin', 'admin')
+        # travis won't allow running the suid binary
+        self.env['ir.config_parameter'].set_param(
+            'report_render_chrome.chrome_extra_parameters', '--no-sandbox'
+        )
         pdf = self.env['report'].get_pdf(
             self.env.ref('base.user_root'), 'report_render_chrome.demo_report',
         )
