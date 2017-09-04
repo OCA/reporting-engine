1ce6fb73c8a8ebcf15c07d3e0f4a6a97680cfebc
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-09-04 16:31:31 +0530

    [MIG] bi_sql_editor v10

--- bi_sql_editor/hooks.py
+++ bi_sql_editor/hooks.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import SUPERUSER_ID
-from openerp.api import Environment
+from odoo import SUPERUSER_ID
+from odoo.api import Environment
 
 
 def uninstall_hook(cr, registry):
