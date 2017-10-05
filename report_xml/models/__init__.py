673e3591840ccac6934adb12ad0a1df856048433
Enric Tobella <etobella@creublanca.es>
2017-10-05 15:32:11 +0200

    [MIG] report_xml: Migration to 11.0

--- report_xml/models/__init__.py
+++ report_xml/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnuorg/licenses/agpl.html).
 
 from . import report_action
-from . import report_generator
--- report_xml/tests/test_report_xml.py
+++ report_xml/tests/test_report_xml.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Creu Blanca
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnuorg/licenses/agpl).
 
 from lxml import etree
 from odoo.tests import common
@@ -8,17 +8,12 @@ from odoo.tests import common
 
 class TestXmlReport(common.TransactionCase):
     def test_xml(self):
-        report_object = self.env['ir.actions.report.xml']
+        report_object = self.env['ir.actions.report']
         report_name = 'report_xml.demo_report_xml_view'
-        self.assertEqual(
-            report_name, report_object._lookup_report(report_name))
+        report = report_object._get_report_from_name(report_name)
         docs = self.env['res.company'].search([], limit=1)
-        rep = report_object.render_report(
-            docs.ids, report_name, {}
-        )
+        self.assertEqual(report.report_type, 'qweb-xml')
+        rep = report.render(docs.ids, {})
         root = etree.fromstring(rep[0])
         el = root.xpath('/root/user/name')
-        self.assertEqual(
-            el[0].text,
-            docs.ensure_one().name
-        )
+        self.assertEqual(el[0].text, docs.ensure_one().name)
