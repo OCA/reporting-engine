c17f6fceb9a33b3335e86dc0a7493ab13ff8aec0
Enric Tobella <etobella@creublanca.es>
2017-10-05 15:32:11 +0200

    [MIG] report_xml: Migration to 11.0

--- report_xml/__init__.py
+++ report_xml/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnuorg/licenses/agpl.html).
 
 from . import controllers
 from . import models
