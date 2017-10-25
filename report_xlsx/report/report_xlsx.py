a2ca54c6a46c3b6918453100173975a3b4fd7768
Enric Tobella <etobella@creublanca.es>
2017-10-25 18:00:03 +0200

    [MIG] report_xlsx: Migration to 11.0

--- report_xlsx/report/report_xlsx.py
+++ report_xlsx/report/report_xlsx.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from cStringIO import StringIO
+from io import BytesIO
 
-from odoo.report.report_sxw import report_sxw
-from odoo.api import Environment
+from odoo import models
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -16,30 +14,17 @@ except ImportError:
     _logger.debug('Can not import xlsxwriter`.')
 
 
-class ReportXlsx(report_sxw):
+class ReportXlsxAbstract(models.AbstractModel):
+    _name = 'report.report_xlsx.abstract'
 
-    def create(self, cr, uid, ids, data, context=None):
-        self.env = Environment(cr, uid, context)
-        report_obj = self.env['ir.actions.report.xml']
-        report = report_obj.search([('report_name', '=', self.name[7:])])
-        if report.ids:
-            self.title = report.name
-            if report.report_type == 'xlsx':
-                return self.create_xlsx_report(ids, data, report)
-        return super(ReportXlsx, self).create(cr, uid, ids, data, context)
-
-    def create_xlsx_report(self, ids, data, report):
-        self.parser_instance = self.parser(
-            self.env.cr, self.env.uid, self.name2, self.env.context)
-        objs = self.getObjects(
-            self.env.cr, self.env.uid, ids, self.env.context)
-        self.parser_instance.set_context(objs, data, ids, 'xlsx')
-        file_data = StringIO()
+    def create_xlsx_report(self, docids, data):
+        objs = self.env[self.env.context.get('active_model')].browse(docids)
+        file_data = BytesIO()
         workbook = xlsxwriter.Workbook(file_data, self.get_workbook_options())
         self.generate_xlsx_report(workbook, data, objs)
         workbook.close()
         file_data.seek(0)
-        return (file_data.read(), 'xlsx')
+        return file_data.read(), 'xlsx'
 
     def get_workbook_options(self):
         return {}
