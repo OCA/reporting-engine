e8ee1f0bc6e8196cedcfa1328ac28b9636fd6244
Alexis de Lattre <alexis.delattre@akretion.com>
2018-02-07 22:11:02 +0100

    Call super() in _get_report_from_name()

--- report_qweb_txt/models/ir_actions_report_xml.py
+++ report_qweb_txt/models/ir_actions_report_xml.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2016-2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class IrActionsReportXml(models.Model):
