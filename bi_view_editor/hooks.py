a403e6e90937b4efe6839efaa4dd6dd668af961a
tarteo <d.sluijk@onestein.nl>
2018-03-22 11:16:32 +0100

    [MIG] bi_view_editor: Migration to 11.0

--- bi_view_editor/hooks.py
+++ bi_view_editor/hooks.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015-2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 
 def uninstall_hook(cr, registry):
