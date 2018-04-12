c210ceb5812d00fc2121463161dec6ec67e455c7
Luc De Meyer <luc.demeyer@noviat.com>
2018-04-12 20:30:20 +0200

    rename module

---
+++ report_xlsx_helper/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2018 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Report xlsx helpers',
+    'author': 'Noviat,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/reporting-engine',
+    'category': 'Reporting',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'external_dependencies': {'python': ['xlsxwriter']},
+    'depends': [
+        'report_xlsx',
+    ],
+    'installable': True,
+}
