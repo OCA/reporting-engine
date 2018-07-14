7c3043b12c73f676eb6f29610b4b6eedb6e8f0f6
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-07-14 11:54:47 -0300

    [9.0] IMP bi_sql_editor (#156)

--- bi_sql_editor/__openerp__.py
+++ bi_sql_editor/__openerp__.py
@@ -6,7 +6,7 @@
 {
     'name': 'BI SQL Editor',
     'summary': "BI Views builder, based on Materialized or Normal SQL Views",
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'license': 'AGPL-3',
     'category': 'Reporting',
     'author': 'GRAP,Odoo Community Association (OCA)',
