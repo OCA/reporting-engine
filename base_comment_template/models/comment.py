c7cc0221dba7ae40788d97b89a2f3e14973c5d04
xavierjimenez <xavier.jimenez@qubiq.es>
2018-07-30 16:55:22 +0200

    [MIG] base_comment_template: Migration to 11.0
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex

--- base_comment_template/models/comment.py
+++ base_comment_template/models/comment.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Guewen Baconnier (Camptocamp SA)
 # Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
