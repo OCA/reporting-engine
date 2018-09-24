84f836f2480296290d73a80794c294c54d7155fa
Luc De Meyer <luc.demeyer@noviat.com>
2018-09-24 17:29:45 +0200

    [11.0][MIG] report_xlsx_helper: Migration to 11.0

--- report_xlsx_helper/__manifest__.py
+++ report_xlsx_helper/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2009-2018 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,9 +7,8 @@
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/reporting-engine',
     'category': 'Reporting',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'external_dependencies': {'python': ['xlsxwriter']},
     'depends': [
         'report_xlsx',
     ],
