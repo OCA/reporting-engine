20994f5b01ddded3d8064d36aab6bfac1fde7d82
David <david.vidal@tecnativa.com>
2018-11-13 09:14:23 +0100

    [MIG] report_qweb_designer: Migration to 12.0

--- report_qweb_signer/models/res_company.py
+++ report_qweb_signer/models/res_company.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingenieria S.L. - Antonio Espinosa
+# Copyright 2015 Tecnativa - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
