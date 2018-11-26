88e53b3ccdbfa02f22e226987d2f0297850466ce
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-11-26 17:58:30 +0100

    [MIG] report_py3o, report_py3o_fusion_server: Migration to 12.0

--- report_py3o/models/py3o_template.py
+++ report_py3o/models/py3o_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 XCG Consulting (http://odoo.consulting)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import fields, models
@@ -6,6 +5,7 @@ from odoo import fields, models
 
 class Py3oTemplate(models.Model):
     _name = 'py3o.template'
+    _description = 'Py3o template'
 
     name = fields.Char(required=True)
     py3o_template_data = fields.Binary("LibreOffice Template")
