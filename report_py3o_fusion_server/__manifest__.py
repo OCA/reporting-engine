8fae3bc1cc70c4e9a4ffd9665b54a2b3db4d8532
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-11-26 17:58:30 +0100

    [MIG] report_py3o, report_py3o_fusion_server: Migration to 12.0

--- report_py3o_fusion_server/__manifest__.py
+++ report_py3o_fusion_server/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Py3o Report Engine - Fusion server support',
     'summary': 'Let the fusion server handle format conversion.',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Reporting',
     'license': 'AGPL-3',
     'author': 'XCG Consulting,'
@@ -23,7 +22,7 @@
         "demo/py3o_pdf_options.xml",
     ],
     'data': [
-        "views/ir_report.xml",
+        "views/ir_actions_report.xml",
         'security/ir.model.access.csv',
         'views/py3o_server.xml',
         'views/py3o_pdf_options.xml',
--- report_py3o_fusion_server/models/__init__.py
+++ report_py3o_fusion_server/models/__init__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import ir_actions_report_xml
+from . import ir_actions_report
 from . import py3o_pdf_options
 from . import py3o_report
 from . import py3o_server
