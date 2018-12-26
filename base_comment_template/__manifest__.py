b984c116653ea53d7aff3770a138c9990a977250
Raf Ven <raf.ven@dynapps.be>
2018-12-26 20:56:21 +0100

    [MIG] base_comment_template: Migration to 12.0

--- base_comment_template/__manifest__.py
+++ base_comment_template/__manifest__.py
@@ -1,15 +1,20 @@
 # © 2013-2014 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-{"name": "Base Comments Templates",
- "summary": "Comments templates on documents",
- "version": "11.0.1.0.0",
- "depends": ["base"],
- "author": "Camptocamp, Odoo Community Association (OCA)",
- "license": "AGPL-3",
- "data": ["views/comment_view.xml",
-          "security/ir.model.access.csv",
-          ],
- "category": "Sale",
- 'installable': True,
- }
+{
+    "name": "Base Comments Templates",
+    "summary": "Comments templates on documents",
+    "version": "12.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/OCA/account-invoice-reporting",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/comment_view.xml",
+    ],
+}
