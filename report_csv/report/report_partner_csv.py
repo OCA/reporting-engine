04df9143fcbc0df6e28d8a309425e4d832b0876c
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-02-05 17:36:12 +0100

    [IMP] report_csv: use dialects

--- report_csv/report/report_partner_csv.py
+++ report_csv/report/report_partner_csv.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
+import csv
 from odoo import models
 
 
@@ -9,13 +9,17 @@ class PartnerCSV(models.AbstractModel):
     _inherit = 'report.report_csv.abstract'
 
     def generate_csv_report(self, writer, data, partners):
+        writer.writeheader()
         for obj in partners:
-            writer.writeheader()
             writer.writerow({
-                'name': obj.name
+                'name': obj.name,
+                'email': obj.email,
             })
 
     def csv_report_options(self):
         res = super().csv_report_options()
         res['fieldnames'].append('name')
+        res['fieldnames'].append('email')
+        res['delimiter'] = ';'
+        res['quoting'] = csv.QUOTE_ALL
         return res
