d0090cce1f75cea995b7ef14666c3a7067f99336
Carlos Dauden <carlos.dauden@tecnativa.com>
2019-03-04 18:09:20 +0100

    [11.0][IMP] account_invoice_report_grouped_by_picking: Over delivery correct price

--- account_invoice_report_grouped_by_picking/__manifest__.py
+++ account_invoice_report_grouped_by_picking/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2017-2019 Carlos Dauden <carlos.dauden@tecnativa.com>
 # Copyright 2018 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Invoice Grouped by Picking",
     "summary": "Print invoice lines grouped by picking",
-    "version": "11.0.1.2.0",
+    "version": "11.0.1.3.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoice-reporting",
     "author": "Tecnativa, "
