54b0ba152998d9835e1c168225323e65c5841a15
Hung Huynh <hunghn.qna@gmail.com>
2019-07-26 22:29:39 +0700

    [ADD] Add parent product.categroy

--- models/product/product_group.py
+++ models/product/product_group.py
@@ -11,3 +11,10 @@ class ProductGroup(models.Model):
     code = fields.Char(string='Code', required=True)
     name = fields.Char(string='Name', required=True)
     note = fields.Text(string='Note')
+    parent_id = fields.Many2one(
+        string='Parent',
+        comodel_name='product.group')
+    child_ids = fields.One2many(
+        string='Children',
+        comodel_name='product.group',
+        inverse_name='parent_id')
