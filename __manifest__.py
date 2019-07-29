ec4016a32a1d594f8c3516580b96d5677263a105
Hung Huynh <hunghn.qna@gmail.com>
2019-07-29 22:28:25 +0700

    [ADD] Initial project

---
+++ __manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Part of hunghn. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Architecture & Construction Project",
+    "version": "1.0",
+    "description": """
+        Application for Architecture & Construction
+    """,
+    "author": "hunghn.erp@gmail.com",
+    "website": "https://vifuna.vn",
+    'category': "Custom",
+    "depends": [
+        "base",
+        'project'
+    ],
+    "data": [
+        # =============== DATA ===================
+
+        # =============== SECURITY ===============
+
+        # =============== VIEWS ==================
+
+        # =============== WIZARDS ================
+
+        # =============== MENU ===================
+
+        # =============== REPORT =================
+
+    ],
+    "qweb": [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# Part of hunghn. See LICENSE file for full copyright and licensing details.
