1203d604726ccdbb64da370e76834ca4f8e626e1
Rod Schouteden <rod.schouteden@dynapps.be>
2019-10-01 10:25:59 +0200

    [MIG] report_xlsx: Migration to 13.0

--- report_xlsx/report/report_partner_xlsx.py
+++ report_xlsx/report/report_partner_xlsx.py
@@ -7,6 +7,7 @@ from odoo import models
 class PartnerXlsx(models.AbstractModel):
     _name = 'report.report_xlsx.partner_xlsx'
     _inherit = 'report.report_xlsx.abstract'
+    _description = 'Partner XLSX Report'
 
     def generate_xlsx_report(self, workbook, data, partners):
         for obj in partners:
