67ecf5c5972ebd90918adaeb5782e7401518e46e
Rod Schouteden <rod.schouteden@dynapps.be>
2019-10-22 09:56:07 +0200

    [MIG][IMP][13.0] rebase to include new linters

--- report_xlsx/tests/test_report.py
+++ report_xlsx/tests/test_report.py
@@ -1,31 +1,32 @@
 # Copyright 2017 Creu Blanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import common
 import logging
+
+from odoo.tests import common
+
 _logger = logging.getLogger(__name__)
 
 try:
     from xlrd import open_workbook
 except ImportError:
-    _logger.debug('Can not import xlrd`.')
+    _logger.debug("Can not import xlrd`.")
 
 
 class TestReport(common.TransactionCase):
     def setUp(self):
         super(TestReport, self).setUp()
-        report_object = self.env['ir.actions.report']
-        self.xlsx_report = (
-            self.env['report.report_xlsx.abstract']
-            .with_context(active_model='res.partner')
+        report_object = self.env["ir.actions.report"]
+        self.xlsx_report = self.env["report.report_xlsx.abstract"].with_context(
+            active_model="res.partner"
         )
-        self.report_name = 'report_xlsx.partner_xlsx'
+        self.report_name = "report_xlsx.partner_xlsx"
         self.report = report_object._get_report_from_name(self.report_name)
-        self.docs = self.env['res.company'].search([], limit=1).partner_id
+        self.docs = self.env["res.company"].search([], limit=1).partner_id
 
     def test_report(self):
         report = self.report
-        self.assertEqual(report.report_type, 'xlsx')
+        self.assertEqual(report.report_type, "xlsx")
         rep = report.render(self.docs.ids, {})
         wb = open_workbook(file_contents=rep[0])
         sheet = wb.sheet_by_index(0)
@@ -35,18 +36,19 @@ class TestReport(common.TransactionCase):
 
         # Typical call from WebUI with wizard
         objs = self.xlsx_report._get_objs_for_report(
-            False, {"context": {"active_ids": self.docs.ids}})
+            False, {"context": {"active_ids": self.docs.ids}}
+        )
         self.assertEquals(objs, self.docs)
 
         # Typical call from within code not to report_action
         objs = self.xlsx_report.with_context(
-            active_ids=self.docs.ids)._get_objs_for_report(False, False)
+            active_ids=self.docs.ids
+        )._get_objs_for_report(False, False)
         self.assertEquals(objs, self.docs)
 
         # Typical call from WebUI
         objs = self.xlsx_report._get_objs_for_report(
-            self.docs.ids,
-            {"data": [self.report_name, self.report.report_type]}
+            self.docs.ids, {"data": [self.report_name, self.report.report_type]}
         )
         self.assertEquals(objs, self.docs)
 
