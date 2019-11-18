4267d990eb832ef121c574d99d4831bd8677cb6f
Robin Goots <robin.goots@dynapps.be>
2019-11-18 10:10:04 +0100

    [MIG] report_qweb_pdf_watermark: Migration to 12.0

--- report_qweb_pdf_watermark/__manifest__.py
+++ report_qweb_pdf_watermark/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Pdf watermark",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Therp BV, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
