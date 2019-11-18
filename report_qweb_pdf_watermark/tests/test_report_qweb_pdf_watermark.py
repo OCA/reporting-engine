51ae03afdf5e6053762c6c46ab366e39753fb266
Robin Goots <robin.goots@dynapps.be>
2019-11-18 10:10:04 +0100

    [MIG] report_qweb_pdf_watermark: Migration to 12.0

--- report_qweb_pdf_watermark/tests/test_report_qweb_pdf_watermark.py
+++ report_qweb_pdf_watermark/tests/test_report_qweb_pdf_watermark.py
@@ -5,26 +5,28 @@ from odoo.tests.common import HttpCase
 
 
 class TestReportQwebPdfWatermark(HttpCase):
-
     def test_report_qweb_pdf_watermark(self):
         Image.init()
         # with our image, we have three
         self._test_report_images(3)
 
-        self.env.ref('report_qweb_pdf_watermark.demo_report').write({
-            'pdf_watermark_expression': False,
-        })
+        self.env.ref("report_qweb_pdf_watermark.demo_report").write(
+            {"pdf_watermark_expression": False}
+        )
         # without, we have two
         self._test_report_images(2)
 
-        self.env.ref('report_qweb_pdf_watermark.demo_report').write({
-            'pdf_watermark': self.env.user.company_id.logo,
-        })
+        self.env.ref("report_qweb_pdf_watermark.demo_report").write(
+            {"pdf_watermark": self.env.user.company_id.logo}
+        )
         # and now we should have three again
         self._test_report_images(3)
 
     def _test_report_images(self, number):
-        report = self.env['ir.model.data'].xmlid_to_object(
-            'report_qweb_pdf_watermark.demo_report')
-        pdf, _ = report.render_qweb_pdf(self.env['res.users'].search([]).ids)
-        self.assertEqual(pdf.count(b'/Subtype /Image'), number)
+        report = self.env["ir.model.data"].xmlid_to_object(
+            "report_qweb_pdf_watermark.demo_report"
+        )
+        pdf, _ = report.with_context(force_report_rendering=True).render_qweb_pdf(
+            self.env["res.users"].search([]).ids
+        )
+        self.assertEqual(pdf.count(b"/Subtype /Image"), number)
