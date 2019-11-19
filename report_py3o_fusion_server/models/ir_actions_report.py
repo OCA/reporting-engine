5bdbf4e09b39d8e866a194bf0632a1ef2c4ddf07
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-19 14:36:37 +0100

    [IMP] report_py3o, report_py3o_fusion_server: black, isort

--- report_py3o_fusion_server/models/ir_actions_report.py
+++ report_py3o_fusion_server/models/ir_actions_report.py
@@ -2,14 +2,16 @@
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
+
 from openerp import _, api, fields, models
+
 from odoo.exceptions import ValidationError
 
 logger = logging.getLogger(__name__)
 
 
 class IrActionsReport(models.Model):
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
     @api.multi
     @api.constrains("py3o_is_local_fusion", "py3o_server_id")
@@ -17,40 +19,52 @@ class IrActionsReport(models.Model):
         for report in self:
             if report.report_type != "py3o":
                 continue
-            if (not report.py3o_is_local_fusion and not report.py3o_server_id):
-                raise ValidationError(_(
-                    "You can not use remote fusion without Fusion server. "
-                    "Please specify a Fusion Server"))
+            if not report.py3o_is_local_fusion and not report.py3o_server_id:
+                raise ValidationError(
+                    _(
+                        "You can not use remote fusion without Fusion server. "
+                        "Please specify a Fusion Server"
+                    )
+                )
 
     py3o_is_local_fusion = fields.Boolean(
         "Local Fusion",
         help="Native formats will be processed without a server. "
-             "You must use this mode if you call methods on your model into "
-             "the template.",
-        default=True)
-    py3o_server_id = fields.Many2one(
-        "py3o.server",
-        "Fusion Server")
+        "You must use this mode if you call methods on your model into "
+        "the template.",
+        default=True,
+    )
+    py3o_server_id = fields.Many2one("py3o.server", "Fusion Server")
     pdf_options_id = fields.Many2one(
-        'py3o.pdf.options', string='PDF Options', ondelete='restrict',
+        "py3o.pdf.options",
+        string="PDF Options",
+        ondelete="restrict",
         help="PDF options can be set per report, but also per Py3o Server. "
-        "If both are defined, the options on the report are used.")
+        "If both are defined, the options on the report are used.",
+    )
 
-    @api.depends("lo_bin_path", "is_py3o_native_format", "report_type",
-                 "py3o_server_id")
+    @api.depends(
+        "lo_bin_path", "is_py3o_native_format", "report_type", "py3o_server_id"
+    )
     @api.multi
     def _compute_py3o_report_not_available(self):
         for rec in self:
             if not rec.report_type == "py3o":
                 continue
-            if (not rec.is_py3o_native_format and
-                    not rec.lo_bin_path and not rec.py3o_server_id):
+            if (
+                not rec.is_py3o_native_format
+                and not rec.lo_bin_path
+                and not rec.py3o_server_id
+            ):
                 rec.is_py3o_report_not_available = True
-                rec.msg_py3o_report_not_available = _(
-                    "A fusion server or a libreoffice runtime are required "
-                    "to genereate the py3o report '%s'. If the libreoffice"
-                    "runtime is already installed and is not found by "
-                    "Odoo, you can provide the full path to the runtime by "
-                    "setting the key 'py3o.conversion_command' into the "
-                    "configuration parameters."
-                ) % rec.name
+                rec.msg_py3o_report_not_available = (
+                    _(
+                        "A fusion server or a libreoffice runtime are required "
+                        "to genereate the py3o report '%s'. If the libreoffice"
+                        "runtime is already installed and is not found by "
+                        "Odoo, you can provide the full path to the runtime by "
+                        "setting the key 'py3o.conversion_command' into the "
+                        "configuration parameters."
+                    )
+                    % rec.name
+                )
--- report_py3o_fusion_server/models/py3o_server.py
+++ report_py3o_fusion_server/models/py3o_server.py
@@ -4,16 +4,21 @@ from odoo import fields, models
 
 
 class Py3oServer(models.Model):
-    _name = 'py3o.server'
-    _description = 'Py3o server'
-    _rec_name = 'url'
+    _name = "py3o.server"
+    _description = "Py3o server"
+    _rec_name = "url"
 
     url = fields.Char(
-        "Py3o Fusion Server URL", required=True,
+        "Py3o Fusion Server URL",
+        required=True,
         help="If your Py3o Fusion server is on the same machine and runs "
-        "on the default port, the URL is http://localhost:8765/form")
+        "on the default port, the URL is http://localhost:8765/form",
+    )
     is_active = fields.Boolean("Active", default=True)
     pdf_options_id = fields.Many2one(
-        'py3o.pdf.options', string='PDF Options', ondelete='restrict',
+        "py3o.pdf.options",
+        string="PDF Options",
+        ondelete="restrict",
         help="PDF options can be set per Py3o Server but also per report. "
-        "If both are defined, the options on the report are used.")
+        "If both are defined, the options on the report are used.",
+    )
