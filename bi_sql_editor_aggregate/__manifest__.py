f81db25020dbd806e373a2523350a8f0b07b4e66
Richard deMeester <richard@willowit.com.au>
2019-11-28 12:15:15 +1100

    [11.0][ENH] sql view - allow group operator
    Replaces #344
    
    Adds the possibility, for float and integer columns, to
    apply a group operator (average, min, max).

---
+++ bi_sql_editor_aggregate/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'BI SQL Editor Aggregate',
+    'summary': 'BI SQL Editor Aggregation',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Reporting',
+    'author': 'Richard deMeester,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/reporting-engine',
+    'depends': [
+        'bi_sql_editor',
+    ],
+    'data': [
+        'views/view_bi_sql_view.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
