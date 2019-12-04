6787910afe63336872f91278d5995adc6a01f99a
Francesco Apruzzese <francescoapruzzese@openforce.it>
2019-12-04 18:00:59 +0100

    [IMP] Show UserError and ValidationError generating excel report

--- report_xlsx/controllers/main.py
+++ report_xlsx/controllers/main.py
@@ -3,39 +3,48 @@
 
 from odoo.addons.web.controllers import main as report
 from odoo.http import content_disposition, route, request
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools.safe_eval import safe_eval
 
 import json
 import time
+import werkzeug
 
 
 class ReportController(report.ReportController):
     @route()
     def report_routes(self, reportname, docids=None, converter=None, **data):
         if converter == 'xlsx':
-            report = request.env['ir.actions.report']._get_report_from_name(
-                reportname)
-            context = dict(request.env.context)
-            if docids:
-                docids = [int(i) for i in docids.split(',')]
-            if data.get('options'):
-                data.update(json.loads(data.pop('options')))
-            if data.get('context'):
-                # Ignore 'lang' here, because the context in data is the one
-                # from the webclient *but* if the user explicitely wants to
-                # change the lang, this mechanism overwrites it.
-                data['context'] = json.loads(data['context'])
-                if data['context'].get('lang'):
-                    del data['context']['lang']
-                context.update(data['context'])
-            xlsx = report.with_context(context).render_xlsx(
-                docids, data=data
-            )[0]
-            report_name = report.report_file
-            if report.print_report_name and not len(docids) > 1:
-                obj = request.env[report.model].browse(docids[0])
-                report_name = safe_eval(report.print_report_name,
-                                        {'object': obj, 'time': time})
+            try:
+                report = request.env['ir.actions.report']._get_report_from_name(
+                    reportname)
+                context = dict(request.env.context)
+                if docids:
+                    docids = [int(i) for i in docids.split(',')]
+                if data.get('options'):
+                    data.update(json.loads(data.pop('options')))
+                if data.get('context'):
+                    # Ignore 'lang' here, because the context in data is the one
+                    # from the webclient *but* if the user explicitely wants to
+                    # change the lang, this mechanism overwrites it.
+                    data['context'] = json.loads(data['context'])
+                    if data['context'].get('lang'):
+                        del data['context']['lang']
+                    context.update(data['context'])
+                xlsx = report.with_context(context).render_xlsx(
+                    docids, data=data
+                )[0]
+                report_name = report.report_file
+                if report.print_report_name and not len(docids) > 1:
+                    obj = request.env[report.model].browse(docids[0])
+                    report_name = safe_eval(report.print_report_name,
+                                            {'object': obj, 'time': time})
+            except (UserError, ValidationError) as odoo_error:
+                raise werkzeug.exceptions.HTTPException(
+                    description='{error_name}. {error_value}'.format(
+                        error_name=odoo_error.name,
+                        error_value=odoo_error.value,
+                    ))
             xlsxhttpheaders = [
                 ('Content-Type', 'application/vnd.openxmlformats-'
                                  'officedocument.spreadsheetml.sheet'),
