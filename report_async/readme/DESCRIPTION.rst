db24cdcc1c78ac73b180b9f7c43f60fb47a7cf4e
kittiu <kittiu@gmail.com>
2019-12-11 00:26:27 +0700

    report_async

---
+++ report_async/readme/DESCRIPTION.rst
@@ -0,0 +1,16 @@
+The new menu "Report Center" is the central place to host your reports in one place.
+From here, there are 2 ways to launch the report,
+
+1. Run Now - run report immediately as per normal.
+2. Run Background - put the report execution to queue job.
+
+By using the queue job, option 2 is great for long running report.
+The report file will be saved for later use, with the option to send report
+by email as soon as it is ready.
+
+Notes:
+
+* Only user with Technical Feature rights can manage the report.
+* Every internal user will have right to execute the report allowed for his/her groups.
+* The files created are owned and viewable only by the person who run the report.
+* Job queue manager can also see all jobs for each reports.
---
+++ report_async/wizard/print_report_wizard.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models, api
+
+
+class PrintReportWizard(models.TransientModel):
+    _name = 'print.report.wizard'
+    _description = 'Print Report Wizard'
+
+    reference = fields.Reference(
+        string='Document',
+        selection='_reference_models',
+        required=True,
+    )
+    action_report_id = fields.Many2one(
+        comodel_name='ir.actions.report',
+        string='Report Template',
+        required=True,
+    )
+
+    @api.model
+    def _reference_models(self):
+        excludes = ['res.company']
+        models = self.env['ir.model'].search([
+            ('state', '!=', 'manual'), ('transient', '=', False),
+            ('model', 'not in', excludes)])
+        return [(model.model, model.name) for model in models]
+
+    @api.onchange('reference')
+    def _onchange_reference(self):
+        self.ensure_one()
+        domain = [('id', 'in', [])]
+        self.action_report_id = False
+        if self.reference:
+            domain = [('model', '=', self.reference._name)]
+        return {'domain': {'action_report_id': domain}}
+
+    @api.multi
+    def print_report(self):
+        self.ensure_one()
+        return self.action_report_id.report_action(self.reference,
+                                                   config=False)
