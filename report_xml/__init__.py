1d7968a3792ca90e4a1421f7cede65171886c12a
Tatiana Deribina <tatiana.deribina@avoin.systems>
2019-12-23 15:49:33 +0200

    [MIG][13.0] report_xml: Migration to 13.0

--- report_xml/__init__.py
+++ report_xml/__init__.py
@@ -2,3 +2,5 @@
 
 from . import controllers
 from . import models
+from . import reports
+from .hooks import post_init_hook
--- report_xml/static/src/js/report/action_manager_report.js
+++ report_xml/static/src/js/report/action_manager_report.js
@@ -1,19 +1,18 @@
-odoo.define('report_xml.ReportActionManager', function (require) {
-    'use strict';
+odoo.define("report_xml.ReportActionManager", function(require) {
+    "use strict";
 
-    var ActionManager = require('web.ActionManager');
+    var ActionManager = require("web.ActionManager");
 
     ActionManager.include({
-        _executeReportAction: function (action, options) {
-            if (action.report_type === 'qweb-xml') {
-                return this._triggerDownload(action, options, 'xml');
+        _executeReportAction: function(action, options) {
+            if (action.report_type === "qweb-xml") {
+                return this._triggerDownload(action, options, "xml");
             }
             return this._super(action, options);
         },
-        _makeReportUrls: function (action) {
+        _makeReportUrls: function(action) {
             var reportUrls = this._super(action);
-            reportUrls.xml = reportUrls.text.replace(
-                '/report/text/', '/report/xml/');
+            reportUrls.xml = reportUrls.text.replace("/report/text/", "/report/xml/");
             return reportUrls;
         },
     });
