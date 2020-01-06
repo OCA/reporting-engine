de2d580af062c955606670388cc2172353991ff5
Nicolas JEUDY <njeudy@myceliandre.fr>
2020-01-06 13:30:37 +0100

    [REF] report_py3o: Black python code

--- report_py3o/models/ir_actions_report.py
+++ report_py3o/models/ir_actions_report.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 try:
     from py3o.formats import Formats
 except ImportError:
-    logger.debug('Cannot import py3o.formats')
+    logger.debug("Cannot import py3o.formats")
 
 PY3O_CONVERSION_COMMAND_PARAMETER = "py3o.conversion_command"
 
@@ -25,15 +25,16 @@ class IrActionsReport(models.Model):
     The list is configurable in the configuration tab, see py3o_template.py
     """
 
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
     @api.multi
     @api.constrains("py3o_filetype", "report_type")
     def _check_py3o_filetype(self):
         for report in self:
             if report.report_type == "py3o" and not report.py3o_filetype:
-                raise ValidationError(_(
-                    "Field 'Output Format' is required for Py3O report"))
+                raise ValidationError(
+                    _("Field 'Output Format' is required for Py3O report")
+                )
 
     @api.model
     def _get_py3o_filetypes(self):
@@ -47,21 +48,17 @@ class IrActionsReport(models.Model):
             selections.append((name, description))
         return selections
 
-    report_type = fields.Selection(
-        selection_add=[("py3o", "py3o")]
-        )
+    report_type = fields.Selection(selection_add=[("py3o", "py3o")])
     py3o_filetype = fields.Selection(
-        selection="_get_py3o_filetypes",
-        string="Output Format")
-    is_py3o_native_format = fields.Boolean(
-        compute='_compute_is_py3o_native_format'
+        selection="_get_py3o_filetypes", string="Output Format"
     )
-    py3o_template_id = fields.Many2one(
-        'py3o.template',
-        "Template")
+    is_py3o_native_format = fields.Boolean(
+        compute="_compute_is_py3o_native_format"
+    )
+    py3o_template_id = fields.Many2one("py3o.template", "Template")
     module = fields.Char(
-        "Module",
-        help="The implementer module that provides this report")
+        "Module", help="The implementer module that provides this report"
+    )
     py3o_template_fallback = fields.Char(
         "Fallback",
         size=128,
@@ -69,24 +66,26 @@ class IrActionsReport(models.Model):
             "If the user does not provide a template this will be used "
             "it should be a relative path to root of YOUR module "
             "or an absolute path on your server."
-        ))
-    report_type = fields.Selection(selection_add=[('py3o', "Py3o")])
+        ),
+    )
+    report_type = fields.Selection(selection_add=[("py3o", "Py3o")])
     py3o_multi_in_one = fields.Boolean(
-        string='Multiple Records in a Single Report',
+        string="Multiple Records in a Single Report",
         help="If you execute a report on several records, "
         "by default Odoo will generate a ZIP file that contains as many "
         "files as selected records. If you enable this option, Odoo will "
-        "generate instead a single report for the selected records.")
+        "generate instead a single report for the selected records.",
+    )
     lo_bin_path = fields.Char(
         string="Path to the libreoffice runtime",
-        compute="_compute_lo_bin_path"
-        )
+        compute="_compute_lo_bin_path",
+    )
     is_py3o_report_not_available = fields.Boolean(
-        compute='_compute_py3o_report_not_available'
-        )
+        compute="_compute_py3o_report_not_available"
+    )
     msg_py3o_report_not_available = fields.Char(
-        compute='_compute_py3o_report_not_available'
-        )
+        compute="_compute_py3o_report_not_available"
+    )
 
     @api.model
     def _register_hook(self):
@@ -106,8 +105,10 @@ class IrActionsReport(models.Model):
 
     @api.model
     def _get_lo_bin(self):
-        lo_bin = self.env['ir.config_parameter'].sudo().get_param(
-            PY3O_CONVERSION_COMMAND_PARAMETER, 'libreoffice',
+        lo_bin = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param(PY3O_CONVERSION_COMMAND_PARAMETER, "libreoffice",)
         )
         try:
             lo_bin = find_in_path(lo_bin)
@@ -139,21 +140,27 @@ class IrActionsReport(models.Model):
                 continue
             if not rec.is_py3o_native_format and not rec.lo_bin_path:
                 rec.is_py3o_report_not_available = True
-                rec.msg_py3o_report_not_available = _(
-                    "The libreoffice runtime is required to genereate the "
-                    "py3o report '%s' but is not found into the bin path. You "
-                    "must install the libreoffice runtime on the server. If "
-                    "the runtime is already installed and is not found by "
-                    "Odoo, you can provide the full path to the runtime by "
-                    "setting the key 'py3o.conversion_command' into the "
-                    "configuration parameters."
-                ) % rec.name
+                rec.msg_py3o_report_not_available = (
+                    _(
+                        "The libreoffice runtime is required to genereate the "
+                        "py3o report '%s' but is not found into the bin path. You "
+                        "must install the libreoffice runtime on the server. If "
+                        "the runtime is already installed and is not found by "
+                        "Odoo, you can provide the full path to the runtime by "
+                        "setting the key 'py3o.conversion_command' into the "
+                        "configuration parameters."
+                    )
+                    % rec.name
+                )
 
     @api.model
     def get_from_report_name(self, report_name, report_type):
         return self.search(
-            [("report_name", "=", report_name),
-             ("report_type", "=", report_type)])
+            [
+                ("report_name", "=", report_name),
+                ("report_type", "=", report_type),
+            ]
+        )
 
     @api.multi
     def render_py3o(self, res_ids, data):
@@ -161,10 +168,13 @@ class IrActionsReport(models.Model):
         if self.report_type != "py3o":
             raise RuntimeError(
                 "py3o rendition is only available on py3o report.\n"
-                "(current: '{}', expected 'py3o'".format(self.report_type))
-        return self.env['py3o.report'].create({
-            'ir_actions_report_id': self.id
-        }).create_report(res_ids, data)
+                "(current: '{}', expected 'py3o'".format(self.report_type)
+            )
+        return (
+            self.env["py3o.report"]
+            .create({"ir_actions_report_id": self.id})
+            .create_report(res_ids, data)
+        )
 
     @api.multi
     def gen_report_download_filename(self, res_ids, data):
@@ -174,8 +184,9 @@ class IrActionsReport(models.Model):
         report = self.get_from_report_name(self.report_name, self.report_type)
         if report.print_report_name and not len(res_ids) > 1:
             obj = self.env[self.model].browse(res_ids)
-            return safe_eval(report.print_report_name,
-                             {'object': obj, 'time': time})
+            return safe_eval(
+                report.print_report_name, {"object": obj, "time": time}
+            )
         return "%s.%s" % (self.name, self.py3o_filetype)
 
     @api.multi
--- report_py3o/models/py3o_report.py
+++ report_py3o/models/py3o_report.py
@@ -23,15 +23,15 @@ try:
     from py3o.template import Template
     from py3o import formats
 except ImportError:
-    logger.debug('Cannot import py3o.template')
+    logger.debug("Cannot import py3o.template")
 try:
     from py3o.formats import Formats, UnkownFormatException
 except ImportError:
-    logger.debug('Cannot import py3o.formats')
+    logger.debug("Cannot import py3o.formats")
 try:
     from PyPDF2 import PdfFileWriter, PdfFileReader
 except ImportError:
-    logger.debug('Cannot import PyPDF2')
+    logger.debug("Cannot import PyPDF2")
 
 _extender_functions = {}
 
@@ -59,12 +59,13 @@ def py3o_report_extender(report_xml_id=None):
     def fct1(fct):
         _extender_functions.setdefault(report_xml_id, []).append(fct)
         return fct
+
     return fct1
 
 
 @py3o_report_extender()
 def default_extend(report_xml, context):
-    context['report_xml'] = report_xml
+    context["report_xml"] = report_xml
 
 
 class Py3oReport(models.TransientModel):
@@ -72,8 +73,7 @@ class Py3oReport(models.TransientModel):
     _description = "Report Py30"
 
     ir_actions_report_id = fields.Many2one(
-        comodel_name="ir.actions.report",
-        required=True
+        comodel_name="ir.actions.report", required=True
     )
 
     @api.multi
@@ -81,18 +81,23 @@ class Py3oReport(models.TransientModel):
         """ Check if the path is a trusted path for py3o templates.
         """
         real_path = os.path.realpath(path)
-        root_path = tools.config.get_misc('report_py3o', 'root_tmpl_path')
+        root_path = tools.config.get_misc("report_py3o", "root_tmpl_path")
         if not root_path:
             logger.warning(
                 "You must provide a root template path into odoo.cfg to be "
                 "able to use py3o template configured with an absolute path "
-                "%s", real_path)
+                "%s",
+                real_path,
+            )
             return False
         is_valid = real_path.startswith(root_path + os.path.sep)
         if not is_valid:
             logger.warning(
                 "Py3o template path is not valid. %s is not a child of root "
-                "path %s", real_path, root_path)
+                "path %s",
+                real_path,
+                root_path,
+            )
         return is_valid
 
     @api.multi
@@ -101,16 +106,16 @@ class Py3oReport(models.TransientModel):
         """
         if filename and os.path.isfile(filename):
             fname, ext = os.path.splitext(filename)
-            ext = ext.replace('.', '')
+            ext = ext.replace(".", "")
             try:
                 fformat = Formats().get_format(ext)
                 if fformat and fformat.native:
                     return True
             except UnkownFormatException:
-                logger.warning("Invalid py3o template %s", filename,
-                               exc_info=1)
-        logger.warning(
-            '%s is not a valid Py3o template filename', filename)
+                logger.warning(
+                    "Invalid py3o template %s", filename, exc_info=1
+                )
+        logger.warning("%s is not a valid Py3o template filename", filename)
         return False
 
     @api.multi
@@ -125,13 +130,12 @@ class Py3oReport(models.TransientModel):
         if report_xml.module:
             # if the default is defined
             flbk_filename = pkg_resources.resource_filename(
-                "odoo.addons.%s" % report_xml.module,
-                tmpl_name,
+                "odoo.addons.%s" % report_xml.module, tmpl_name,
             )
         elif self._is_valid_template_path(tmpl_name):
             flbk_filename = os.path.realpath(tmpl_name)
         if self._is_valid_template_filename(flbk_filename):
-            with open(flbk_filename, 'rb') as tmpl:
+            with open(flbk_filename, "rb") as tmpl:
                 return tmpl.read()
         return None
 
@@ -173,8 +177,7 @@ class Py3oReport(models.TransientModel):
         if tmpl_data is None:
             # if for any reason the template is not found
             raise TemplateNotFound(
-                _('No template found. Aborting.'),
-                sys.exc_info(),
+                _("No template found. Aborting."), sys.exc_info(),
             )
 
         return tmpl_data
@@ -197,20 +200,21 @@ class Py3oReport(models.TransientModel):
         context.update(
             report_xml._get_rendering_context(model_instance.ids, data)
         )
-        context['objects'] = model_instance
+        context["objects"] = model_instance
         self._extend_parser_context(context, report_xml)
         return context
 
     @api.multi
     def _postprocess_report(self, model_instance, result_path):
         if len(model_instance) == 1 and self.ir_actions_report_id.attachment:
-            with open(result_path, 'rb') as f:
+            with open(result_path, "rb") as f:
                 # we do all the generation process using files to avoid memory
                 # consumption...
                 # ... but odoo wants the whole data in memory anyways :)
                 buffer = BytesIO(f.read())
                 self.ir_actions_report_id.postprocess_pdf_report(
-                    model_instance, buffer)
+                    model_instance, buffer
+                )
         return result_path
 
     @api.multi
@@ -219,18 +223,19 @@ class Py3oReport(models.TransientModel):
         """
         self.ensure_one()
         result_fd, result_path = tempfile.mkstemp(
-            suffix='.ods', prefix='p3o.report.tmp.')
+            suffix=".ods", prefix="p3o.report.tmp."
+        )
         tmpl_data = self.get_template(model_instance)
 
         in_stream = BytesIO(tmpl_data)
-        with closing(os.fdopen(result_fd, 'wb+')) as out_stream:
+        with closing(os.fdopen(result_fd, "wb+")) as out_stream:
             template = Template(in_stream, out_stream, escape_false=True)
             localcontext = self._get_parser_context(model_instance, data)
             template.render(localcontext)
             out_stream.seek(0)
             tmpl_data = out_stream.read()
 
-        if self.env.context.get('report_py3o_skip_conversion'):
+        if self.env.context.get("report_py3o_skip_conversion"):
             return result_path
 
         result_path = self._convert_single_report(
@@ -246,18 +251,20 @@ class Py3oReport(models.TransientModel):
             command = self._convert_single_report_cmd(
                 result_path, model_instance, data,
             )
-            logger.debug('Running command %s', command)
+            logger.debug("Running command %s", command)
             output = subprocess.check_output(
                 command, cwd=os.path.dirname(result_path),
             )
-            logger.debug('Output was %s', output)
+            logger.debug("Output was %s", output)
             self._cleanup_tempfiles([result_path])
             result_path, result_filename = os.path.split(result_path)
             result_path = os.path.join(
-                result_path, '%s.%s' % (
+                result_path,
+                "%s.%s"
+                % (
                     os.path.splitext(result_filename)[0],
-                    self.ir_actions_report_id.py3o_filetype
-                )
+                    self.ir_actions_report_id.py3o_filetype,
+                ),
             )
         return result_path
 
@@ -267,43 +274,48 @@ class Py3oReport(models.TransientModel):
         lo_bin = self.ir_actions_report_id.lo_bin_path
         if not lo_bin:
             raise RuntimeError(
-                _("Libreoffice runtime not available. "
-                  "Please contact your administrator.")
+                _(
+                    "Libreoffice runtime not available. "
+                    "Please contact your administrator."
+                )
             )
         return [
             lo_bin,
-            '--headless',
-            '--convert-to',
+            "--headless",
+            "--convert-to",
             self.ir_actions_report_id.py3o_filetype,
             result_path,
         ]
 
     @api.multi
-    def _get_or_create_single_report(self, model_instance, data,
-                                     existing_reports_attachment):
+    def _get_or_create_single_report(
+        self, model_instance, data, existing_reports_attachment
+    ):
         self.ensure_one()
-        attachment = existing_reports_attachment.get(
-            model_instance.id)
+        attachment = existing_reports_attachment.get(model_instance.id)
         if attachment and self.ir_actions_report_id.attachment_use:
             content = base64.decodestring(attachment.datas)
             report_file = tempfile.mktemp(
-                "." + self.ir_actions_report_id.py3o_filetype)
+                "." + self.ir_actions_report_id.py3o_filetype
+            )
             with open(report_file, "wb") as f:
                 f.write(content)
             return report_file
-        return self._create_single_report(
-            model_instance, data)
+        return self._create_single_report(model_instance, data)
 
     @api.multi
     def _zip_results(self, reports_path):
         self.ensure_one()
         zfname_prefix = self.ir_actions_report_id.name
-        result_path = tempfile.mktemp(suffix="zip", prefix='py3o-zip-result')
-        with ZipFile(result_path, 'w', ZIP_DEFLATED) as zf:
+        result_path = tempfile.mktemp(suffix="zip", prefix="py3o-zip-result")
+        with ZipFile(result_path, "w", ZIP_DEFLATED) as zf:
             cpt = 0
             for report in reports_path:
                 fname = "%s_%d.%s" % (
-                    zfname_prefix, cpt, report.split('.')[-1])
+                    zfname_prefix,
+                    cpt,
+                    report.split(".")[-1],
+                )
                 zf.write(report, fname)
 
                 cpt += 1
@@ -321,8 +333,9 @@ class Py3oReport(models.TransientModel):
             reader = PdfFileReader(path)
             writer.appendPagesFromReader(reader)
         merged_file_fd, merged_file_path = tempfile.mkstemp(
-            suffix='.pdf', prefix='report.merged.tmp.')
-        with closing(os.fdopen(merged_file_fd, 'wb')) as merged_file:
+            suffix=".pdf", prefix="report.merged.tmp."
+        )
+        with closing(os.fdopen(merged_file_fd, "wb")) as merged_file:
             writer.write(merged_file)
         return merged_file_path
 
@@ -337,7 +350,7 @@ class Py3oReport(models.TransientModel):
         if filetype == formats.FORMAT_PDF:
             return self._merge_pdf(reports_path), formats.FORMAT_PDF
         else:
-            return self._zip_results(reports_path), 'zip'
+            return self._zip_results(reports_path), "zip"
 
     @api.model
     def _cleanup_tempfiles(self, temporary_files):
@@ -347,28 +360,31 @@ class Py3oReport(models.TransientModel):
                 os.unlink(temporary_file)
             except (OSError, IOError):
                 logger.error(
-                    'Error when trying to remove file %s' % temporary_file)
+                    "Error when trying to remove file %s" % temporary_file
+                )
 
     @api.multi
     def create_report(self, res_ids, data):
         """ Override this function to handle our py3o report
         """
         model_instances = self.env[self.ir_actions_report_id.model].browse(
-            res_ids)
+            res_ids
+        )
         reports_path = []
-        if (
-                len(res_ids) > 1 and
-                self.ir_actions_report_id.py3o_multi_in_one):
+        if len(res_ids) > 1 and self.ir_actions_report_id.py3o_multi_in_one:
             reports_path.append(
-                self._create_single_report(
-                    model_instances, data))
+                self._create_single_report(model_instances, data)
+            )
         else:
-            existing_reports_attachment = \
-                self.ir_actions_report_id._get_attachments(res_ids)
+            existing_reports_attachment = self.ir_actions_report_id._get_attachments(
+                res_ids
+            )
             for model_instance in model_instances:
                 reports_path.append(
                     self._get_or_create_single_report(
-                        model_instance, data, existing_reports_attachment))
+                        model_instance, data, existing_reports_attachment
+                    )
+                )
 
         result_path, filetype = self._merge_results(reports_path)
         reports_path.append(result_path)
@@ -378,7 +394,7 @@ class Py3oReport(models.TransientModel):
         # consumption...
         # ... but odoo wants the whole data in memory anyways :)
 
-        with open(result_path, 'r+b') as fd:
+        with open(result_path, "r+b") as fd:
             res = fd.read()
         self._cleanup_tempfiles(set(reports_path))
         return res, filetype
