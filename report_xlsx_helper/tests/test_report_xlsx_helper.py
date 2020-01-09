f33e647a26d8839ee16472ed7eaddc625770b7be
kongrattapong <rattapong.chok@gmail.com>
2020-01-09 11:26:17 +0700

    [IMP] report_xlsx_helper: black, isort

--- report_xlsx_helper/tests/test_report_xlsx_helper.py
+++ report_xlsx_helper/tests/test_report_xlsx_helper.py
@@ -7,22 +7,21 @@ from odoo.tests.common import TransactionCase
 
 
 class TestReportXlsxHelper(TransactionCase):
-
     def setUp(self):
         super(TestReportXlsxHelper, self).setUp()
         today = date.today()
-        p1 = self.env.ref('base.res_partner_1')
-        p2 = self.env.ref('base.res_partner_2')
+        p1 = self.env.ref("base.res_partner_1")
+        p2 = self.env.ref("base.res_partner_2")
         p1.date = today
         p2.date = today
         self.partners = p1 + p2
         ctx = {
-            'report_name': 'report_xlsx_helper.test_partner_xlsx',
-            'active_model': 'res.partner',
-            'active_ids': self.partners.ids,
+            "report_name": "report_xlsx_helper.test_partner_xlsx",
+            "active_model": "res.partner",
+            "active_ids": self.partners.ids,
         }
-        self.report = self.env['ir.actions.report'].with_context(ctx)
+        self.report = self.env["ir.actions.report"].with_context(ctx)
 
     def test_report_xlsx_helper(self):
         report_xls = self.report.render_xlsx(None, None)
-        self.assertEqual(report_xls[1], 'xlsx')
+        self.assertEqual(report_xls[1], "xlsx")
