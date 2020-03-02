4849eca77a35855c871ea9cb30b296d2bde84d1f
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-02 10:59:46 +0100

    [IMP] report_qr: black, isort

--- report_qr/__manifest__.py
+++ report_qr/__manifest__.py
@@ -2,17 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Web QR Manager',
-    'version': '12.0.1.0.0',
-    'author': 'Creu Blanca, '
-              'Odoo Community Association (OCA)',
-    'category': 'Sales',
-    'website': 'https://github.com/OCA/reporting-engine',
-    'license': 'AGPL-3',
-    'depends': [
-        'web',
-    ],
-    'data': [
-    ],
-    'installable': True,
+    "name": "Web QR Manager",
+    "version": "12.0.1.0.0",
+    "author": "Creu Blanca, " "Odoo Community Association (OCA)",
+    "category": "Sales",
+    "website": "https://github.com/OCA/reporting-engine",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "data": [],
+    "installable": True,
 }
