d0907b8385911b6f0f9cb238efe3a1503becf8ab
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-12 15:41:31 +0100

    [FIX] base_comment_template: Make field truly company dependent
    Previous field declaration was incorrect due to a typo, leading to non applying the
    company dependent features.
    
    Now everything is correct and the migration scripts care of moving data properly.

--- base_comment_template/migrations/12.0.3.0.0/post-migration.py
+++ base_comment_template/migrations/12.0.3.0.0/post-migration.py
@@ -5,9 +5,10 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
+    cr = env.cr
     table = 'res_partner'
-    old_column = 'property_comment_template_id'
-    new_column = openupgrade.get_legacy_name(old_column)
-    if openupgrade.column_exists(cr, table, new_column):
+    new_column = 'property_comment_template_id'
+    old_column = openupgrade.get_legacy_name(new_column)
+    if openupgrade.column_exists(cr, table, old_column):
         openupgrade.convert_to_company_dependent(
-            env, 'res.partner', old_column, new_column, 'res.partner')
+            env, 'res.partner', old_column, new_column, 'res_partner')
