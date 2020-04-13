3e876ace30eb664e3f4cd262486aa8482303115a
Bhavesh Odedra <bodedra@ursainfosystems.com>
2020-04-13 11:18:24 +0530

    [13.0][MIG] report_batch

--- report_batch/__manifest__.py
+++ report_batch/__manifest__.py
@@ -5,13 +5,13 @@
 {
     "name": "Batch Report Printing",
     "summary": "Ability to print multiple QWeb reports in a single batch.",
-    "version": "11.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Reporting",
     "website": "http://www.opensourceintegrators.com",
-    "depends": ["stock",],
-    "data": ["security/ir.model.access.csv", "views/ir_action_report_view.xml",],
+    "depends": ["stock"],
+    "data": ["security/ir.model.access.csv", "views/ir_action_report_view.xml"],
     "installable": True,
     "maintainers": ["bodedra"],
 }
