0e8043663e4ce2d0af76608d71e74b78f55b49e1
Bhavesh Odedra <bodedra@ursainfosystems.com>
2020-04-13 11:17:00 +0530

    [IMP] report_batch: black, isort, prettier

--- report_batch/__manifest__.py
+++ report_batch/__manifest__.py
@@ -3,20 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Batch Report Printing',
-    'summary': 'Ability to print multiple QWeb reports in a single batch.',
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
-    'category': 'Reporting',
-    'website': 'http://www.opensourceintegrators.com',
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/ir_action_report_view.xml',
-    ],
-    'installable': True,
-    'maintainers': ['bodedra'],
+    "name": "Batch Report Printing",
+    "summary": "Ability to print multiple QWeb reports in a single batch.",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Reporting",
+    "website": "http://www.opensourceintegrators.com",
+    "depends": ["stock",],
+    "data": ["security/ir.model.access.csv", "views/ir_action_report_view.xml",],
+    "installable": True,
+    "maintainers": ["bodedra"],
 }
