17f5daefc8bd78daec376e28c0dc6ffeefc51023
Enric Tobella <etobella@creublanca.es>
2020-04-14 09:16:25 +0200

    [12.0][ADD] kpi_dashboard

---
+++ kpi_dashboard/static/src/js/dashboard_model.js
@@ -0,0 +1,23 @@
+odoo.define('kpi_dashboard.DashboardModel', function (require) {
+    "use strict";
+
+    var BasicModel = require('web.BasicModel');
+
+    var DashboardModel = BasicModel.extend({
+        _fetchRecord: function (record, options) {
+            return this._rpc({
+                model: record.model,
+                method: 'read_dashboard',
+                args: [[record.res_id]],
+                context: _.extend({}, record.getContext(), {bin_size: true}),
+            })
+            .then(function (result) {
+                record.specialData = result;
+                return result
+            })
+        }
+    });
+
+    return DashboardModel;
+
+});
