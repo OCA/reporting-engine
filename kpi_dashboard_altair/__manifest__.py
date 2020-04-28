7246ea6027a2e69b31f8c8c15d8aa2744c55a3a0
Enric Tobella <etobella@creublanca.es>
2020-04-28 15:16:10 +0200

    [12.0][ADD] kpi_dashboard_altair

---
+++ kpi_dashboard_altair/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Kpi Dashboard Altair",
+    "summary": """
+        Create dashboards using altair""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/reporting-engine",
+    "depends": ["kpi_dashboard"],
+    "data": ["views/webclient_templates.xml"],
+    "qweb": ["static/src/xml/dashboard.xml"],
+    "external_dependencies": {
+        "python": ['altair'],
+    },
+    "demo": ["demo/demo_dashboard_altair.xml"],
+    "maintainers": ["etobella"],
+}
---
+++ kpi_dashboard_altair/models/__init__.py
@@ -0,0 +1,1 @@
+from . import kpi_kpi
