1e230c86ee6606c9ce07b7aee9b0be2bcbfc4a80
Saran440 <saranl@ecosoft.co.th>
2020-05-15 11:52:40 +0700

    [13.0][MIG] report_wkhtmltopdf_param

--- report_wkhtmltopdf_param/__manifest__.py
+++ report_wkhtmltopdf_param/__manifest__.py
@@ -4,19 +4,18 @@
 # noinspection PyStatementEffect
 {
     "name": "Report Wkhtmltopdf Param",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "summary": """
         Add new parameters for a paper format to be used by wkhtmltopdf
         command as arguments.
     """,
-    "author": "Avoin.Systems," "Eficent," "Odoo Community Association (OCA)",
-    "website": "https://avoin.systems",
+    "author": "Avoin.Systems, Eficent, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/reporting-engine",
     "category": "Technical Settings",
-    "depends": ["web",],
-    "data": ["security/ir.model.access.csv", "views/paperformat.xml",],
+    "depends": ["web"],
+    "data": ["security/ir.model.access.csv", "views/paperformat.xml"],
     "installable": True,
     "auto_install": False,
     "application": False,
-    "active": False,
 }
