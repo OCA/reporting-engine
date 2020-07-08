5829ca477d1a8efeb75f3aa8eb42dc5229e7f1f4
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-07-08 10:36:28 +0300

    [IMP] base_comment_template: black, isort, prettier

--- base_comment_template/migrations/12.0.3.0.0/pre-migration.py
+++ base_comment_template/migrations/12.0.3.0.0/pre-migration.py
@@ -6,8 +6,8 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     cr = env.cr
-    table = 'res_partner'
-    old_column = 'property_comment_template_id'
+    table = "res_partner"
+    old_column = "property_comment_template_id"
     new_column = openupgrade.get_legacy_name(old_column)
     if openupgrade.column_exists(cr, table, old_column):
         openupgrade.rename_columns(cr, {table: [(old_column, new_column)]})
