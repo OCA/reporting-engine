a5057c020bc2f462faa000897eaa0c9475209db1
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-30 12:06:33 +0200

    [IMP] report_context: black, isort, prettier

--- report_context/controllers/main.py
+++ report_context/controllers/main.py
@@ -1,18 +1,20 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo.addons.web.controllers import main as report
-from odoo.http import route, request
 import json
+
+from odoo.http import request, route
+
+from odoo.addons.web.controllers import main as report
 
 
 class ReportController(report.ReportController):
     @route()
     def report_routes(self, reportname, docids=None, converter=None, **data):
-        report = request.env['ir.actions.report']._get_report_from_name(
-            reportname)
-        original_context = json.loads(data.get('context', '{}'))
-        data['context'] = json.dumps(report.with_context(
-            original_context
-        )._get_context())
+        report = request.env["ir.actions.report"]._get_report_from_name(reportname)
+        original_context = json.loads(data.get("context", "{}"))
+        data["context"] = json.dumps(
+            report.with_context(original_context)._get_context()
+        )
         return super().report_routes(
-            reportname, docids=docids, converter=converter, **data)
+            reportname, docids=docids, converter=converter, **data
+        )
--- report_context/tests/test_report_context.py
+++ report_context/tests/test_report_context.py
@@ -4,44 +4,47 @@ from odoo.tests.common import TransactionCase
 
 
 class TestReportContext(TransactionCase):
-
     def test_report_01(self):
-        company = self.browse_ref('base.main_company')
-        report = self.browse_ref('web.action_report_internalpreview')
-        self.env['ir.config_parameter'].sudo().set_param(
-            'report.default.context', '{"test_parameter": 1}')
-        report.write({'context': '{"test_parameter": 2}'})
+        company = self.browse_ref("base.main_company")
+        report = self.browse_ref("web.action_report_internalpreview")
+        self.env["ir.config_parameter"].sudo().set_param(
+            "report.default.context", '{"test_parameter": 1}'
+        )
+        report.write({"context": '{"test_parameter": 2}'})
         action = report.with_context(test_parameter=3).report_action(company)
-        self.assertEqual(3, action['context']['test_parameter'])
+        self.assertEqual(3, action["context"]["test_parameter"])
 
     def test_report_02(self):
-        company = self.browse_ref('base.main_company')
-        report = self.browse_ref('web.action_report_internalpreview')
-        self.env['ir.config_parameter'].sudo().set_param(
-            'report.default.context', '{"test_parameter": 1}')
-        report.write({'context': '{"test_parameter": 2}'})
+        company = self.browse_ref("base.main_company")
+        report = self.browse_ref("web.action_report_internalpreview")
+        self.env["ir.config_parameter"].sudo().set_param(
+            "report.default.context", '{"test_parameter": 1}'
+        )
+        report.write({"context": '{"test_parameter": 2}'})
         action = report.report_action(company)
-        self.assertEqual(2,  action['context']['test_parameter'])
+        self.assertEqual(2, action["context"]["test_parameter"])
 
     def test_report_03(self):
-        company = self.browse_ref('base.main_company')
-        report = self.browse_ref('web.action_report_internalpreview')
-        self.env['ir.config_parameter'].sudo().set_param(
-            'report.default.context', '{"test_parameter": 1}')
+        company = self.browse_ref("base.main_company")
+        report = self.browse_ref("web.action_report_internalpreview")
+        self.env["ir.config_parameter"].sudo().set_param(
+            "report.default.context", '{"test_parameter": 1}'
+        )
         action = report.with_context(test_parameter=3).report_action(company)
-        self.assertEqual(3, action['context']['test_parameter'])
+        self.assertEqual(3, action["context"]["test_parameter"])
 
     def test_report_04(self):
-        company = self.browse_ref('base.main_company')
-        report = self.browse_ref('web.action_report_internalpreview')
-        report.write({'context': '{"test_parameter": 2}'})
+        company = self.browse_ref("base.main_company")
+        report = self.browse_ref("web.action_report_internalpreview")
+        report.write({"context": '{"test_parameter": 2}'})
         action = report.report_action(company)
-        self.assertEqual(2, action['context']['test_parameter'])
+        self.assertEqual(2, action["context"]["test_parameter"])
 
     def test_report_05(self):
-        company = self.browse_ref('base.main_company')
-        report = self.browse_ref('web.action_report_internalpreview')
-        self.env['ir.config_parameter'].sudo().set_param(
-            'report.default.context', '{"test_parameter": 1}')
+        company = self.browse_ref("base.main_company")
+        report = self.browse_ref("web.action_report_internalpreview")
+        self.env["ir.config_parameter"].sudo().set_param(
+            "report.default.context", '{"test_parameter": 1}'
+        )
         action = report.report_action(company)
-        self.assertEqual(1, action['context']['test_parameter'])
+        self.assertEqual(1, action["context"]["test_parameter"])
