99bb8ac22c63a6d04544165969d9484384b2947a
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-30 12:30:43 +0200

    [13.0][MIG] report_context

--- report_context/tests/test_report_context.py
+++ report_context/tests/test_report_context.py
@@ -12,7 +12,9 @@ class TestReportContext(TransactionCase):
         )
         report.write({"context": '{"test_parameter": 2}'})
         action = report.with_context(test_parameter=3).report_action(company)
-        self.assertEqual(3, action["context"]["test_parameter"])
+        self.assertEqual(
+            3, action["context"]["report_action"]["context"]["test_parameter"]
+        )
 
     def test_report_02(self):
         company = self.browse_ref("base.main_company")
@@ -22,7 +24,9 @@ class TestReportContext(TransactionCase):
         )
         report.write({"context": '{"test_parameter": 2}'})
         action = report.report_action(company)
-        self.assertEqual(2, action["context"]["test_parameter"])
+        self.assertEqual(
+            2, action["context"]["report_action"]["context"]["test_parameter"]
+        )
 
     def test_report_03(self):
         company = self.browse_ref("base.main_company")
@@ -31,14 +35,18 @@ class TestReportContext(TransactionCase):
             "report.default.context", '{"test_parameter": 1}'
         )
         action = report.with_context(test_parameter=3).report_action(company)
-        self.assertEqual(3, action["context"]["test_parameter"])
+        self.assertEqual(
+            3, action["context"]["report_action"]["context"]["test_parameter"]
+        )
 
     def test_report_04(self):
         company = self.browse_ref("base.main_company")
         report = self.browse_ref("web.action_report_internalpreview")
         report.write({"context": '{"test_parameter": 2}'})
         action = report.report_action(company)
-        self.assertEqual(2, action["context"]["test_parameter"])
+        self.assertEqual(
+            2, action["context"]["report_action"]["context"]["test_parameter"]
+        )
 
     def test_report_05(self):
         company = self.browse_ref("base.main_company")
@@ -47,4 +55,6 @@ class TestReportContext(TransactionCase):
             "report.default.context", '{"test_parameter": 1}'
         )
         action = report.report_action(company)
-        self.assertEqual(1, action["context"]["test_parameter"])
+        self.assertEqual(
+            1, action["context"]["report_action"]["context"]["test_parameter"]
+        )
