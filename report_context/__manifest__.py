a7cc7a188b69c60e560d0090a9c4bed3985d4269
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-30 12:30:43 +0200

    [13.0][MIG] report_context

--- report_context/__manifest__.py
+++ report_context/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Report Context",
     "summary": """
         Adding context to reports""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/reporting-engine",
--- report_context/models/ir_actions_report.py
+++ report_context/models/ir_actions_report.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.tools.safe_eval import safe_eval
 
 
@@ -29,13 +29,11 @@ class IrActionsReport(models.Model):
         context.update(self.env.context)
         return context
 
-    @api.multi
     def render(self, res_ids, data=None):
         return super(IrActionsReport, self.with_context(self._get_context())).render(
             res_ids, data=data
         )
 
-    @api.noguess
     def report_action(self, docids, data=None, config=True):
         return super(
             IrActionsReport, self.with_context(self._get_context())
