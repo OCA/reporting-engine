57aee07bc004b757793b4b27606d8af73fc1e5bf
Andrea <a.stirpe@onestein.nl>
2020-10-20 09:12:20 +0200

    pre-commit

--- bi_view_editor/static/src/js/bi_view_editor.js
+++ bi_view_editor/static/src/js/bi_view_editor.js
@@ -1,7 +1,7 @@
 /* Copyright 2015-2019 Onestein (<https://www.onestein.eu>)
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("bi_view_editor", function(require) {
+odoo.define("bi_view_editor", function (require) {
     "use strict";
 
     var JoinNodeDialog = require("bi_view_editor.JoinNodeDialog");
@@ -17,32 +17,32 @@ odoo.define("bi_view_editor", function(require) {
         events: {
             "click .clear-btn": "clear",
         },
-        start: function() {
+        start: function () {
             var self = this;
             var res = this._super.apply(this, arguments);
 
             // Init ModelList
             this.model_list = new ModelList(this);
             this.model_list.appendTo(this.$(".body > .left"));
-            this.model_list.on("field_clicked", this, function(field) {
+            this.model_list.on("field_clicked", this, function (field) {
                 self.addField(_.extend({}, field));
             });
 
             // Init FieldList
             this.field_list = new FieldList(this);
             this.field_list.appendTo(this.$(".body > .right")).then(
-                function() {
+                function () {
                     this.field_list.on("removed", this, this.fieldListRemoved);
                     this.field_list.on("updated", this, this.fieldListChanged);
                     this.$el.find(".body > .right").droppable({
                         accept: "div.class-list div.field",
-                        drop: function(event, ui) {
+                        drop: function (event, ui) {
                             self.addField(_.extend({}, ui.draggable.data("field")));
                             ui.draggable.draggable("option", "revert", false);
                         },
                     });
 
-                    this.on("change:effective_readonly", this, function() {
+                    this.on("change:effective_readonly", this, function () {
                         this.updateMode();
                     });
                     this.renderValue();
@@ -53,31 +53,31 @@ odoo.define("bi_view_editor", function(require) {
 
             return res;
         },
-        clear: function() {
+        clear: function () {
             if (this.mode !== "readonly") {
                 this.field_list.set([]);
                 this.loadAndPopulateModelList();
                 this._setValue(this.field_list.get());
             }
         },
-        fieldListChanged: function() {
+        fieldListChanged: function () {
             this._setValue(this.field_list.get());
         },
-        fieldListRemoved: function() {
+        fieldListRemoved: function () {
             this._setValue(this.field_list.get());
             var model = new Data.DataSet(this, "bve.view");
             model.call("get_clean_list", [this.lastSetValue]).then(
-                function(result) {
+                function (result) {
                     this.field_list.set(JSON.parse(result));
                     this._setValue(this.field_list.get());
                 }.bind(this)
             );
             this.loadAndPopulateModelList();
         },
-        renderValue: function() {
+        renderValue: function () {
             this.field_list.set(JSON.parse(this.value));
         },
-        updateMode: function() {
+        updateMode: function () {
             if (this.mode === "readonly") {
                 this.$el.find(".clear-btn").addClass("d-none");
                 this.$el.find(".body .right").droppable("option", "disabled", true);
@@ -88,18 +88,18 @@ odoo.define("bi_view_editor", function(require) {
             this.field_list.setMode(this.mode);
             this.model_list.setMode(this.mode);
         },
-        loadAndPopulateModelList: function() {
+        loadAndPopulateModelList: function () {
             var model_ids = null;
             if (this.field_list.get().length > 0) {
                 model_ids = this.field_list.getModelIds();
             }
             this.model_list.loadModels(model_ids).then(
-                function(models) {
+                function (models) {
                     this.model_list.populateModels(models);
                 }.bind(this)
             );
         },
-        getTableAlias: function(field) {
+        getTableAlias: function (field) {
             if (typeof field.table_alias === "undefined") {
                 var model_ids = this.field_list.getModelIds();
                 var n = 1;
@@ -110,7 +110,7 @@ odoo.define("bi_view_editor", function(require) {
             }
             return field.table_alias;
         },
-        addFieldAndJoinNode: function(field, join_node) {
+        addFieldAndJoinNode: function (field, join_node) {
             if (join_node.join_node === -1 || join_node.table_alias === -1) {
                 field.table_alias = this.getTableAlias(field);
                 if (join_node.join_node === -1) {
@@ -127,12 +127,12 @@ odoo.define("bi_view_editor", function(require) {
             this.loadAndPopulateModelList();
             this._setValue(this.field_list.get());
         },
-        addField: function(field) {
+        addField: function (field) {
             var data = _.extend({}, field);
             var model = new Data.DataSet(this, "ir.model");
             var field_data = this.field_list.get();
             model.call("get_join_nodes", [field_data, data]).then(
-                function(result) {
+                function (result) {
                     if (result.length === 1) {
                         this.addFieldAndJoinNode(data, result[0]);
                     } else if (result.length > 1) {
@@ -142,7 +142,7 @@ odoo.define("bi_view_editor", function(require) {
                             result,
                             this.field_list.getModelData()
                         );
-                        dialog.open().on("chosen", this, function(e) {
+                        dialog.open().on("chosen", this, function (e) {
                             this.addFieldAndJoinNode(data, e.choice);
                         });
                     } else {
@@ -154,7 +154,7 @@ odoo.define("bi_view_editor", function(require) {
                 }.bind(this)
             );
         },
-        _parseValue: function(value) {
+        _parseValue: function (value) {
             return JSON.stringify(value);
         },
     });
