96f7091eeb0915c672e353550eab9881a238b7ab
Carlos Roca <carlos.roca@tecnativa.com>
2020-10-28 10:23:07 +0100

    [MIG] report_qweb_parameter: Migration to v13

--- report_qweb_parameter/__manifest__.py
+++ report_qweb_parameter/__manifest__.py
@@ -3,13 +3,13 @@
 
 {
     "name": "Report QWeb Parameter",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "summary": """
         Add new parameters for qweb templates in order to reduce field length
         and check minimal length
     """,
-    "author": "Creu Blanca," "Odoo Community Association (OCA)",
+    "author": "Creu Blanca, Odoo Community Association (OCA)",
     "website": "https://github.com/oca/reporting-engine",
     "category": "Technical Settings",
     "depends": ["web"],
---
+++ report_qweb_parameter/readme/USAGE.rst
@@ -0,0 +1,3 @@
+#. Add a t-length attribute on report templates fields that will truncate the field
+#. Add a t-minlength attribute on report template fields that will check the min length
+#. Add a t-maxlength attribute on report template fields that will check the max length
