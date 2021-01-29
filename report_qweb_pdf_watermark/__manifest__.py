010b6ec39d3260e29e75e230e494dc99ae34624c
fshah <foram.shah@initos.com>
2021-01-29 15:30:18 +0530

    [IMP] report_qweb_pdf_watermark: black, isort, prettier

--- report_qweb_pdf_watermark/__manifest__.py
+++ report_qweb_pdf_watermark/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Pdf watermark",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Therp BV, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Technical Settings",
     "summary": "Add watermarks to your QWEB PDF reports",
-    "website": "https://github.com/oca/reporting-engine",
+    "website": "https://github.com/OCA/reporting-engine",
     "depends": ["web"],
     "data": [
         "demo/report.xml",
