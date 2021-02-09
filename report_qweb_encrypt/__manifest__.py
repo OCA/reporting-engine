575e639437855e50347433a2bdfa7d1852b60dc7
Kitti U <kittiu@gmail.com>
2021-02-10 00:12:38 +0700

    [14.0][MIG] report_qweb_encrypt

--- report_qweb_encrypt/__manifest__.py
+++ report_qweb_encrypt/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Report Qweb Encrypt",
     "summary": "Allow to encrypt qweb pdfs",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Ecosoft,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/reporting-engine",
