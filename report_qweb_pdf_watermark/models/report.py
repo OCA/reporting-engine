65aed37f0c8b4da14b27c3a6cb36e69fec1e3403
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-02-15 09:21:10 +0100

    moved getting image to method in order to be able to change actual behavior. added image resizing for A4 format

--- report_qweb_pdf_watermark/models/report.py
+++ report_qweb_pdf_watermark/models/report.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import math
+from StringIO import StringIO
 from base64 import b64decode
 from logging import getLogger
+
 from PIL import Image
-from StringIO import StringIO
 from pyPdf import PdfFileWriter, PdfFileReader
 from pyPdf.utils import PdfReadError
+
 try:
     from PyPDF2 import PdfFileWriter, PdfFileReader  # pylint: disable=W0404
     from PyPDF2.utils import PdfReadError  # pylint: disable=W0404
@@ -18,34 +21,27 @@ try:
 except ImportError:
     pass
 from openerp import api, models, tools
+
 logger = getLogger(__name__)
+
+paper_mm = {
+    u'A4': (210, 297)
+}
 
 
 class Report(models.Model):
     _inherit = 'report'
 
     @api.v7
-    def get_pdf(
-        self, cr, uid, ids, report_name, html=None, data=None, context=None
-    ):
+    def get_pdf(self, cr, uid, ids, report_name, html=None, data=None, context=None):
         # pylint: disable=R8110
         # this override cannot be done in v8 api
         result = super(Report, self).get_pdf(
             cr, uid, ids, report_name, html=html, data=data,
             context=context
         )
-        report = self._get_report_from_name(cr, uid, report_name)
-        watermark = None
-        if report.pdf_watermark:
-            watermark = b64decode(report.pdf_watermark)
-        else:
-            env = api.Environment(cr, uid, context)
-            watermark = tools.safe_eval(
-                report.pdf_watermark_expression or 'None',
-                dict(env=env, docs=env[report.model].browse(ids)),
-            )
-            if watermark:
-                watermark = b64decode(watermark)
+
+        report, watermark = self.get_watermark(cr, uid, ids, report_name, context)
 
         if not watermark:
             return result
@@ -61,12 +57,22 @@ class Report(models.Model):
                 pdf_buffer = StringIO()
                 if image.mode != 'RGB':
                     image = image.convert('RGB')
-                resolution = image.info.get(
-                    'dpi', report.paperformat_id.dpi or 90
+                dpi = image.info.get(
+                    'dpi', report.paperformat_id.dpi or 300
                 )
-                if isinstance(resolution, tuple):
-                    resolution = resolution[0]
-                image.save(pdf_buffer, 'pdf', resolution=resolution)
+                if isinstance(dpi, tuple):
+                    dpi = dpi[0]
+
+                # resizing image based paper format millimeters
+                # formula: dpi * mm * 0,0393701 inches
+                mm = paper_mm.get(report.paperformat_id.format)
+                if mm:
+                    image_dimension = tuple(
+                        int(math.ceil(dim * dpi * 0.0393701)) for dim in mm
+                    )
+                    image = image.resize(image_dimension, Image.ANTIALIAS)
+
+                image.save(pdf_buffer, 'pdf', resolution=dpi)
                 pdf_watermark = PdfFileReader(pdf_buffer)
             except:
                 logger.exception('Failed to load watermark')
@@ -95,3 +101,21 @@ class Report(models.Model):
         pdf.write(pdf_content)
 
         return pdf_content.getvalue()
+
+    def get_watermark(self, cr, uid, ids, report_name, context):
+        """
+        by default the watermark is getting from report. inherit to change this
+        behaviour
+        """
+        report = self._get_report_from_name(cr, uid, report_name)
+        if report.pdf_watermark:
+            watermark = b64decode(report.pdf_watermark)
+        else:
+            env = api.Environment(cr, uid, context)
+            watermark = tools.safe_eval(
+                report.pdf_watermark_expression or 'None',
+                dict(env=env, docs=env[report.model].browse(ids)),
+            )
+            if watermark:
+                watermark = b64decode(watermark)
+        return report, watermark
--- report_qweb_pdf_watermark/tests/test_report_qweb_pdf_watermark.py
+++ report_qweb_pdf_watermark/tests/test_report_qweb_pdf_watermark.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import base64
+import os
+
 from PIL import Image
 from openerp.tests.common import HttpCase
 
@@ -9,6 +12,15 @@ class TestReportQwebPdfWatermark(HttpCase):
     def test_report_qweb_pdf_watermark(self):
         Image.init()
         # with our image, we have three
+
+        logo = self.env.user.company_id.logo
+        if not logo:
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            file_path = os.path.join(dir_path, '../static/description/icon.png')
+            fn = open(file_path, 'r')
+            self.env.user.company_id.logo = base64.encodestring(fn.read())
+            fn.close()
+
         self._test_report_images(3)
 
         self.env.ref('report_qweb_pdf_watermark.demo_report').write({
