34333c421641c39397b03bd49889e8fca3131484
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-05-15 16:58:39 +0200

    [FIX] base_comment_template: Incorrect m2m column names + script + avoid FK constraint
    - The name of the columns were swapped, so better to fix it for avoiding
      mistakes due to this.
    - Migration script for detecting the condition and swap column names.
    - On v12 > v13 migration, include JOINs for avoiding FK constraint,
      which `ON CONFLICT` clause doesn't protect.

--- base_comment_template/migrations/14.0.1.0.0/pre-migration.py
+++ base_comment_template/migrations/14.0.1.0.0/pre-migration.py
@@ -1,7 +1,10 @@
-# Copyright 2020 NextERP Romania SRL
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
+
+field_renames = [
+    ("base.comment.template", "base_comment_template", "priority", "sequence"),
+]
 
 
 @openupgrade.migrate()
@@ -16,10 +19,8 @@ def migrate(env, version):
         SPLIT_PART(ip.value_reference, ',', 2)::int AS base_comment_template_id
         FROM ir_property ip
         JOIN ir_model_fields imf ON ip.fields_id = imf.id
-        JOIN res_partner rp ON rp.id = SPLIT_PART(ip.res_id, ',', 2)::int
-        JOIN base_comment_template bct
-            ON bct.id = SPLIT_PART(ip.value_reference, ',', 2)::int
         WHERE imf.name = 'property_comment_template_id'
         AND imf.model = 'res.partner'
         """,
     )
+    openupgrade.rename_fields(env, field_renames)
