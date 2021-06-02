bfe7267c4a1929ab33ba8f993cf241722b107303
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-06-02 14:49:15 +0200

    report_xlsx: bump development status [ci skip]

--- report_xlsx/__manifest__.py
+++ report_xlsx/__manifest__.py
@@ -7,6 +7,7 @@
     "website": "https://github.com/OCA/reporting-engine",
     "category": "Reporting",
     "version": "14.0.1.0.1",
+    "development_status": "Production/Stable",
     "license": "AGPL-3",
     "external_dependencies": {"python": ["xlsxwriter", "xlrd"]},
     "depends": ["base", "web"],
