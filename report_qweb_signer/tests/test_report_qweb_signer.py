40dac0db847308577d46939b9407b2ec8900f6d8
Laurent-Corron <laurent.corron@acsone.eu>
2021-06-29 10:16:32 +0200

    [MIG] report_qweb_signer: Migration to 13.0
    [UPD] Update report_qweb_signer.pot
    
    [UPD] README.rst

--- report_qweb_signer/tests/test_report_qweb_signer.py
+++ report_qweb_signer/tests/test_report_qweb_signer.py
@@ -7,9 +7,7 @@ from odoo.tests.common import HttpCase
 class TestReportQwebSigner(HttpCase):
     def setUp(self):
         super(TestReportQwebSigner, self).setUp()
-        self.partner = self.env["res.partner"].create(
-            {"name": "Test partner", "customer": True}
-        )
+        self.partner = self.env["res.partner"].create({"name": "Test partner"})
         self.report = self.env.ref(
             "report_qweb_signer.partner_demo_report"
         ).with_context(force_report_rendering=True)
