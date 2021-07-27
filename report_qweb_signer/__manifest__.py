176ce7f41b08bb3f007d10fcd77736fa347c8e75
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-07-27 15:26:50 +0200

    Apply dotfiles

--- report_qweb_signer/__manifest__.py
+++ report_qweb_signer/__manifest__.py
@@ -6,9 +6,9 @@
 {
     "name": "Qweb PDF reports signer",
     "summary": "Sign Qweb PDFs usign a PKCS#12 certificate",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Reporting",
-    "website": "https://github.com/oca/reporting-engine",
+    "website": "https://github.com/OCA/reporting-engine",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
