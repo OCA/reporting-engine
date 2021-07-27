6c9814822f04749cd525b2207945d632a9b74eb6
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-07-27 15:26:50 +0200

    Apply dotfiles

--- report_csv/__manifest__.py
+++ report_csv/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Base report csv",
     "summary": "Base module to create csv report",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
-    "website": "https://github.com/oca/reporting-engine",
+    "website": "https://github.com/OCA/reporting-engine",
     "category": "Reporting",
     "version": "13.0.1.0.1",
     "license": "AGPL-3",
