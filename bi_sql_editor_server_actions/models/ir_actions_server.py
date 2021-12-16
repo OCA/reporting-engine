4bc2c952c72003d58cd4a95307aa5953f4848f97
Valentin <valentin.nagy@haulogy.net>
2021-12-16 09:44:42 +0100

    bi_sql_editor_server_actions precommit - generate setup files

--- bi_sql_editor_server_actions/models/ir_actions_server.py
+++ bi_sql_editor_server_actions/models/ir_actions_server.py
@@ -2,7 +2,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class IrActionsServer(models.Model):
--- bi_sql_editor_server_actions/tests/test_bi_sql_view_server_action.py
+++ bi_sql_editor_server_actions/tests/test_bi_sql_view_server_action.py
@@ -1,7 +1,6 @@
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.exceptions import AccessError, UserError
 from odoo.tests.common import SingleTransactionCase, at_install, post_install
 
 
