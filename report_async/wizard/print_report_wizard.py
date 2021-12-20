e822e1655aa7fa1daf1142a9cb65e76cb226ffd8
newtratip <tharathip.chaweewongphan@gmail.com>
2021-12-20 15:29:29 +0700

    report_async: black, isort, prettier

--- report_async/wizard/print_report_wizard.py
+++ report_async/wizard/print_report_wizard.py
@@ -1,43 +1,45 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class PrintReportWizard(models.TransientModel):
-    _name = 'print.report.wizard'
-    _description = 'Print Report Wizard'
+    _name = "print.report.wizard"
+    _description = "Print Report Wizard"
 
     reference = fields.Reference(
-        string='Document',
-        selection='_reference_models',
+        string="Document",
+        selection="_reference_models",
         required=True,
     )
     action_report_id = fields.Many2one(
-        comodel_name='ir.actions.report',
-        string='Report Template',
+        comodel_name="ir.actions.report",
+        string="Report Template",
         required=True,
     )
 
     @api.model
     def _reference_models(self):
-        excludes = ['res.company']
-        models = self.env['ir.model'].search([
-            ('state', '!=', 'manual'), ('transient', '=', False),
-            ('model', 'not in', excludes)])
+        excludes = ["res.company"]
+        models = self.env["ir.model"].search(
+            [
+                ("state", "!=", "manual"),
+                ("transient", "=", False),
+                ("model", "not in", excludes),
+            ]
+        )
         return [(model.model, model.name) for model in models]
 
-    @api.onchange('reference')
+    @api.onchange("reference")
     def _onchange_reference(self):
         self.ensure_one()
-        domain = [('id', 'in', [])]
+        domain = [("id", "in", [])]
         self.action_report_id = False
         if self.reference:
-            domain = [('model', '=', self.reference._name)]
-        return {'domain': {'action_report_id': domain}}
+            domain = [("model", "=", self.reference._name)]
+        return {"domain": {"action_report_id": domain}}
 
-    @api.multi
     def print_report(self):
         self.ensure_one()
-        return self.action_report_id.report_action(self.reference,
-                                                   config=False)
+        return self.action_report_id.report_action(self.reference, config=False)
