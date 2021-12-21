deaf8f577d4959fe0bd9e3c253e8e24f358356f7
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2021-12-21 16:01:31 +0100

    [REF] report_py3o_signer: move pdf signing in _postprocess_report

--- report_py3o_signer/models/ir_actions_report.py
+++ report_py3o_signer/models/ir_actions_report.py
@@ -2,9 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
-import os
-import tempfile
-from contextlib import closing
 
 from odoo import models
 
@@ -12,45 +9,8 @@ _logger = logging.getLogger(__name__)
 
 
 class IrActionReport(models.Model):
-
     _inherit = "ir.actions.report"
 
     def _is_report_type_signable(self):
         res = super()._is_report_type_signable()
         return res or (self.report_type == "py3o")
-
-    def render_py3o(self, res_ids, data):
-        certificate = self._certificate_get(res_ids)
-        content, filetype = super().render_py3o(res_ids, data)
-        # don't try to sign if report in attachment
-        if self.attachment_use and self._get_attachments(res_ids):
-            return content, filetype
-        signed = ""
-        if certificate:
-            # Creating temporary origin PDF
-            pdf_fd, pdf = tempfile.mkstemp(suffix=".pdf", prefix="report.tmp.")
-            try:
-                with closing(os.fdopen(pdf_fd, "wb")) as pf:
-                    pf.write(content)
-                _logger.debug(
-                    "Signing PDF document '%s' for IDs %s with certificate '%s'",
-                    self.report_name,
-                    res_ids,
-                    certificate.name,
-                )
-                signed = self.pdf_sign(pdf, certificate)
-                # Read signed PDF
-                if os.path.exists(signed):
-                    with open(signed, "rb") as pf:
-                        content = pf.read()
-                if len(res_ids) == 1:
-                    model_instance = self.env[self.model].browse(res_ids)
-                    self.postprocess_pdf_report(model_instance, content)
-            finally:
-                # Manual cleanup of the temporary files
-                for fname in (pdf, signed):
-                    try:
-                        os.unlink(fname)
-                    except (OSError, IOError):
-                        _logger.error("Error when trying to remove file %s", fname)
-        return content, filetype
