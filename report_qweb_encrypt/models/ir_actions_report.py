8a0ed07a17a137bce2c3d102292016500a5adc12
ps-tubtim <pimolnats@ecosoft.co.th>
2022-01-07 11:25:50 +0700

    [FIX] report_qweb_encrypt: conflict with bank statement

--- report_qweb_encrypt/models/ir_actions_report.py
+++ report_qweb_encrypt/models/ir_actions_report.py
@@ -34,6 +34,8 @@ class IrActionsReport(models.Model):
         document, ttype = super(IrActionsReport, self)._render_qweb_pdf(
             res_ids=res_ids, data=data
         )
+        if isinstance(res_ids, int):
+            res_ids = [res_ids]
         password = self._get_pdf_password(res_ids[:1])
         document = self._encrypt_pdf(document, password)
         return document, ttype
