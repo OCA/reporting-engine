153dabf9cb1f90cb085a101144bc3b8e8645109b
Mihai Fekete <feketemihai@nexterp.ro>
2022-01-10 16:49:29 +0200

    [14.0] [FIX] Fix base comment template rendering.
    [UPD] Update base_comment_template.pot
    
    [UPD] README.rst
    
    base_comment_template 14.0.3.0.0
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: reporting-engine-14.0/reporting-engine-14.0-base_comment_template
    Translate-URL: https://translation.odoo-community.org/projects/reporting-engine-14-0/reporting-engine-14-0-base_comment_template/

--- base_comment_template/__manifest__.py
+++ base_comment_template/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Comments Templates",
     "summary": "Add conditional mako template to any report"
     "on models that inherits comment.template.",
-    "version": "14.0.2.0.1",
+    "version": "14.0.3.0.0",
     "category": "Reporting",
     "website": "https://github.com/OCA/reporting-engine",
     "author": "Camptocamp, Odoo Community Association (OCA)",
@@ -15,6 +15,7 @@
     "data": [
         "security/ir.model.access.csv",
         "security/security.xml",
+        "wizard/base_comment_template_preview_views.xml",
         "views/base_comment_template_view.xml",
         "views/res_partner_view.xml",
     ],
