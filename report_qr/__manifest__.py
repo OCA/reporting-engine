2c9b72af72a9686dfeb59adc588f8c3c71cb1a9f
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-02-07 16:04:16 +0530

    [MIG] report_qr: Migration to 15.0

--- report_qr/__manifest__.py
+++ report_qr/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Web QR Manager",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "author": "Creu Blanca, " "Odoo Community Association (OCA)",
     "category": "Reporting",
     "website": "https://github.com/OCA/reporting-engine",
