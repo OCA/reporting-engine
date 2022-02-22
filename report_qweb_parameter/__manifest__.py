41fcda22c2cab9546edb0bc4943406300a102f70
AlexanderAcysos <100213934+alexanderacysos@users.noreply.github.com>
2022-02-22 17:15:00 +0100

    [MIG] report_qweb_parameter: Migration to 15.0

--- report_qweb_parameter/__manifest__.py
+++ report_qweb_parameter/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Report QWeb Parameter",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "summary": """
         Add new parameters for qweb templates in order to reduce field length
