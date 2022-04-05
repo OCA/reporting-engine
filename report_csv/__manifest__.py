a26c17f08e2c05c465138e16aa0dec89e5ad8756
Adrien Peiffer <adrien.peiffer@acsone.eu>
2022-04-05 13:46:25 +0200

    [MIG] report_csv: Migration to 15.0

--- report_csv/__manifest__.py
+++ report_csv/__manifest__.py
@@ -9,7 +9,11 @@
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base", "web"],
-    "data": ["views/webclient_templates.xml"],
     "demo": ["demo/report.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "report_csv/static/src/js/report/qwebactionmanager.esm.js"
+        ]
+    },
     "installable": True,
 }
