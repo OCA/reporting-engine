43842693a3a602ea0746b6cc338458444bd7edb1
Adrien Peiffer <adrien.peiffer@acsone.eu>
2022-04-05 13:46:25 +0200

    [MIG] report_csv: Migration to 15.0

--- report_csv/controllers/main.py
+++ report_csv/controllers/main.py
@@ -2,11 +2,22 @@
 # License AGPL-3.0 or later (https://www.gnuorg/licenses/agpl.html).
 
 import json
+import logging
 
-from odoo.http import content_disposition, request, route
-from odoo.tools.safe_eval import safe_eval
+from werkzeug.urls import url_decode
+
+from odoo.http import (
+    content_disposition,
+    request,
+    route,
+    serialize_exception as _serialize_exception,
+)
+from odoo.tools import html_escape
+from odoo.tools.safe_eval import safe_eval, time
 
 from odoo.addons.web.controllers import main as report
+
+_logger = logging.getLogger(__name__)
 
 
 class ReportController(report.ReportController):
@@ -28,29 +39,65 @@ class ReportController(report.ReportController):
                     del data["context"]["lang"]
                 context.update(data["context"])
             csv = report.with_context(**context)._render_csv(docids, data=data)[0]
-            filename = "{}.{}".format(report.name, "csv")
-            if docids:
-                obj = request.env[report.model].browse(docids)
-                if report.print_report_name and not len(obj) > 1:
-                    report_name = safe_eval(
-                        report.print_report_name,
-                        {"object": obj},
-                    )
-                    filename = "{}.{}".format(report_name, "csv")
-                # When we print multiple records we still allow a custom
-                # filename.
-                elif report.print_report_name and len(obj) > 1:
-                    report_name = safe_eval(
-                        report.print_report_name,
-                        {"object": obj},
-                    )
-                    filename = "{}.{}".format(report_name, "csv")
             csvhttpheaders = [
                 ("Content-Type", "text/csv"),
                 ("Content-Length", len(csv)),
-                ("Content-Disposition", content_disposition(filename)),
             ]
             return request.make_response(csv, headers=csvhttpheaders)
         return super(ReportController, self).report_routes(
             reportname, docids, converter, **data
         )
+
+    @route()
+    def report_download(self, data, context=None):
+        requestcontent = json.loads(data)
+        url, report_type = requestcontent[0], requestcontent[1]
+        try:
+            if report_type == "csv":
+                reportname = url.split("/report/csv/")[1].split("?")[0]
+                docids = None
+                if "/" in reportname:
+                    reportname, docids = reportname.split("/")
+                if docids:
+                    # Generic report:
+                    response = self.report_routes(
+                        reportname, docids=docids, converter="csv", context=context
+                    )
+                else:
+                    # Particular report:
+                    data = dict(
+                        url_decode(url.split("?")[1]).items()
+                    )  # decoding the args represented in JSON
+                    if "context" in data:
+                        context, data_context = json.loads(context or "{}"), json.loads(
+                            data.pop("context")
+                        )
+                        context = json.dumps({**context, **data_context})
+                    response = self.report_routes(
+                        reportname, converter="csv", context=context, **data
+                    )
+
+                report = request.env["ir.actions.report"]._get_report_from_name(
+                    reportname
+                )
+                filename = "%s.%s" % (report.name, "csv")
+
+                if docids:
+                    ids = [int(x) for x in docids.split(",")]
+                    obj = request.env[report.model].browse(ids)
+                    if report.print_report_name and not len(obj) > 1:
+                        report_name = safe_eval(
+                            report.print_report_name, {"object": obj, "time": time}
+                        )
+                        filename = "%s.%s" % (report_name, "csv")
+                response.headers.add(
+                    "Content-Disposition", content_disposition(filename)
+                )
+                return response
+            else:
+                return super(ReportController, self).report_download(data, context)
+        except Exception as e:
+            _logger.exception("Error while generating report %s", reportname)
+            se = _serialize_exception(e)
+            error = {"code": 200, "message": "Odoo Server Error", "data": se}
+            return request.make_response(html_escape(json.dumps(error)))
