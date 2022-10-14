84ba7c8bd1853e9065e97d4ce3d68103e247e7b8
Adam Heinz <amh@metricwise.net>
2022-10-14 11:29:11 -0400

    [MIG] report_batch: Migration to 14.0

--- report_batch/__manifest__.py
+++ report_batch/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Batch Report Printing",
     "summary": "Ability to print multiple QWeb reports in a single batch.",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Reporting",
