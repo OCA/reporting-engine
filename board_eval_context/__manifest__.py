bf4565bdfd4e736801a6222ddd29cd74480e7e44
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2023-02-06 08:51:23 +0100

    [ADD] module board_eval_context
    This module adds some useful keys in the evaluation context of board.board
    records, which can be useful when creating generic boards for all the users of
    an instance.
    
    At the moment it allows using the following values in the domains of a board.board record:
    
    * datetime: the datetime.datetime class
    * date: the datetime.date class
    * timedelta: the datetime.timedelta class
    * timezone: the datetime.timezone class
    * tzinfo: the datetime.tzinfo class
    * relativedelta: the dateutil.relativedelta.relativedelta class
    * uid: the ID of the current user

---
+++ board_eval_context/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Board Eval Context",
+    "summary": "Add some keys to board.board eval context",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainer": "gurneyalex",
+    "depends": [
+        "board",
+    ],
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/reporting-engine",
+    "installable": True,
+    "auto_install": False,
+    "license": "AGPL-3",
+    "application": False,
+    "development_status": "Alpha",
+    "maintainers": ["gurneyalex"],
+}
