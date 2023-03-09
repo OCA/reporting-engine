404f24e23d1ecaa98a402e1e5fb146f02ca8a662
Miquel Raïch <miquel.raich@forgeflow.com>
2023-03-09 18:10:05 +0100

    [FIX] partner_statement: use partner lang in xlsx reports

--- partner_statement/report/detailed_activity_statement_xlsx.py
+++ partner_statement/report/detailed_activity_statement_xlsx.py
@@ -388,6 +388,8 @@ class DetailedActivityStatementXslx(models.AbstractModel):
             sheet.set_column(0, i, 20)
 
     def generate_xlsx_report(self, workbook, data, objects):
+        lang = objects.lang or self.env.user.partner_id.lang
+        self = self.with_context(lang=lang)
         report_model = self.env["report.partner_statement.detailed_activity_statement"]
         self._define_formats(workbook)
         FORMATS["format_distributed"] = workbook.add_format({"align": "vdistributed"})
--- partner_statement/report/outstanting_statement_xlsx.py
+++ partner_statement/report/outstanting_statement_xlsx.py
@@ -185,6 +185,8 @@ class OutstandingStatementXslx(models.AbstractModel):
             sheet.set_column(0, i, 20)
 
     def generate_xlsx_report(self, workbook, data, objects):
+        lang = objects.lang or self.env.user.partner_id.lang
+        self = self.with_context(lang=lang)
         report_model = self.env["report.partner_statement.outstanding_statement"]
         self._define_formats(workbook)
         FORMATS["format_distributed"] = workbook.add_format({"align": "vdistributed"})
