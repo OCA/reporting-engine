fe281ec4b8058459baa73690141408f0fc69e4b6
anjeel.haria <anjeel.haria@brainvire.com>
2023-03-13 15:11:27 +0100

    [MIG] report_qweb_pdf_watermark: Migration to 16.0
    Syntax Fixes

--- report_qweb_pdf_watermark/__manifest__.py
+++ report_qweb_pdf_watermark/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Pdf watermark",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Technical Settings",
--- report_qweb_pdf_watermark/tests/test_report_qweb_pdf_watermark.py
+++ report_qweb_pdf_watermark/tests/test_report_qweb_pdf_watermark.py
@@ -1,4 +1,5 @@
 # © 2016 Therp BV <http://therp.nl>
+# Copyright 2023 Onestein - Anjeel Haria
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from PIL import Image
 
@@ -36,9 +37,13 @@ class TestReportQwebPdfWatermark(HttpCase):
         self._test_report_images(3)
 
     def _test_report_images(self, number):
-        report = self.env.ref("report_qweb_pdf_watermark.demo_report")
-        pdf, _ = report.with_context(force_report_rendering=True)._render_qweb_pdf(
-            self.env["res.users"].search([]).ids
+        pdf, _ = (
+            self.env["ir.actions.report"]
+            .with_context(force_report_rendering=True)
+            ._render_qweb_pdf(
+                "report_qweb_pdf_watermark.demo_report",
+                self.env["res.users"].search([]).ids,
+            )
         )
         self.assertEqual(pdf.count(b"/Subtype /Image"), number)
 
