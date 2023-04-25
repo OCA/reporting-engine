7f0bf744a22cbbc34742a1ec13861e266b29d7e2
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-25 11:28:00 +0200

    [ADD] report_qweb_custom_renderer

---
+++ report_qweb_custom_renderer/models/ir_actions_report.py
@@ -0,0 +1,44 @@
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import logging
+import time
+
+from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class IrActionsReport(models.Model):
+    _inherit = "ir.actions.report"
+
+    qweb_pdf_engine = fields.Selection(
+        [("wkhtmltopdf", "wkhtmltopdf")],
+        default="wkhtmltopdf",
+        string="PDF Engine",
+    )
+
+    @api.multi
+    def render_qweb_pdf(self, res_ids=None, data=None):
+        qweb_pdf_engine = "wkhtmltopdf"
+        qweb_pdf_engine = "weasyprint"
+
+        _logger.info("========================================")
+        _logger.info("=================<begin> render_qweb_pdf")
+        _logger.info("========================================")
+        time_1 = time.time()
+        if qweb_pdf_engine == "wkhtmltopdf":
+            result = super(IrActionsReport, self).render_qweb_pdf(
+                res_ids=res_ids,
+                data=data,
+            )
+        else:
+            result = getattr(self, "_render_qweb_pdf_%s" % qweb_pdf_engine)(
+                res_ids=res_ids,
+                data=data,
+            )
+        time_2 = time.time()
+        _logger.info("%s: %s seconds" % (qweb_pdf_engine, time_2 - time_1))
+        _logger.info("========================================")
+        _logger.info("=================<end> render_qweb_pdf")
+        _logger.info("========================================")
+        return result
