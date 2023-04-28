a745102f2f3ef157ab37c8445fb5a7c7836f772c
sistecem <info@sistecem.com>
2023-04-28 09:03:51 -0400

    Update CONFIGURE.rst

--- report_qweb_signer/__manifest__.py
+++ report_qweb_signer/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Qweb PDF reports signer",
     "summary": "Sign Qweb PDFs usign a PKCS#12 certificate",
-    "version": "14.0.2.0.1",
+    "version": "14.0.2.0.2",
     "category": "Reporting",
     "website": "https://github.com/OCA/reporting-engine",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
