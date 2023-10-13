c0b1078028bccd5beb88881e0452f2a5398961d3
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2023-10-13 15:18:14 +0200

    [ADD]report_xlsx_boilerplate New addon to have the possibility to add Excel boilerplate templates. This can be useful for those reports where we want to always have a predetermined structure in which we then want to add more information. Example: we always now that the Column Headers will be: Header 1, Header 2, ... We can add the Boilerplate with the Headers, and from there, in the `generate_xlsx_report` method, we will already have the predefined Excel so that we can edit on top of it.

---
+++ report_xlsx_boilerplate/report/utils/xlsx_utils.py
@@ -0,0 +1,19 @@
+# Documentations used:
+#   Openpyxl: https://openpyxl.readthedocs.io/
+#   XlsxWriter: https://xlsxwriter.readthedocs.io/
+HORIZ_ALIG_ACPTD_VALS = {
+    "left": "left",
+    "center": "center",
+    "right": "right",
+    "fill": "fill",
+    "justify": "justify",
+    "centerContinuous": "center_across",
+    "distributed": "distributed",
+}
+VERT_ALIG_ACPTD_VALS = {
+    "top": "top",
+    "center": "vcenter",
+    "bottom": "bottom",
+    "justify": "vjustify",
+    "distributed": "vdistributed",
+}
