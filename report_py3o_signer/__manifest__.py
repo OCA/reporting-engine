80caeadcdbad1caa1dfeedbe838817fc297d6d51
Benjamin Willig <benjamin.willig@acsone.eu>
2024-01-10 10:50:58 +0100

    [MIG] report_py3o_signer to 16.0

--- report_py3o_signer/__manifest__.py
+++ report_py3o_signer/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Report Py3o Signer",
     "summary": """
         Sign Py3o PDF's using PKCS#12 certificate""",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Acsone SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/reporting-engine",
