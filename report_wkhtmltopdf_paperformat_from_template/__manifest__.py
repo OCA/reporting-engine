764329a2b8fd6b51796449123aa439e4d2eb8f7b
Iván Todorovich <ivan.todorovich@gmail.com>
2024-02-29 12:51:38 -0300

    [ADD] report_wkhtmltopdf_paperformat_from_template

---
+++ report_wkhtmltopdf_paperformat_from_template/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Camptocamp SA (https://www.camptocamp.com).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Report Wkhtmltopdf Paperformat from Template",
+    "summary": "Force the paperformat directly in the specific paperformat args",
+    "version": "16.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/OCA/reporting-engine",
+    "license": "AGPL-3",
+    "category": "Technical",
+    "depends": ["web"],
+    "data": ["views/report_layout.xml"],
+}
