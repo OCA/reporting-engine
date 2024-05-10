a7a2c50eecdf313d370617abad922b4f3037d0c5
Sakthivel <sakthivel@sodexis.com>
2024-05-10 13:32:07 +0530

    [IMP] report_async: pre-commit auto fixes

--- report_async/__manifest__.py
+++ report_async/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Report Async",
     "summary": "Central place to run reports live or async",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/reporting-engine",
