df36b6ed3f9f55663404bea709aad645a766ab34
Mmequignon <matthieu@fwzte.xyz>
2024-12-19 16:32:08 +0100

    report_xlsx: Fix currency format

--- report_xlsx/tests/test_report.py
+++ report_xlsx/tests/test_report.py
@@ -59,9 +59,5 @@ class TestReport(common.TransactionCase):
     def test_currency_format(self):
         usd = self.env.ref("base.USD")
         self.assertEqual(
-            self.xlsx_report._report_xlsx_currency_format(usd), "$#,##0.00"
-        )
-        eur = self.env.ref("base.EUR")
-        self.assertEqual(
-            self.xlsx_report._report_xlsx_currency_format(eur), "#,##0.00 €"
+            self.xlsx_report._report_xlsx_currency_format(usd), "#,##0.00"
         )
