d1ebd94f91588ea92661bd4ae99acd08c1eb3937
len <len@lambdao.dev>
2025-03-10 10:48:50 +0100

    [MIG] report_py3o_signer

--- report_py3o_signer/models/ir_actions_report.py
+++ report_py3o_signer/models/ir_actions_report.py
@@ -1,27 +1,32 @@
 # Copyright 2023 fah-mili/Lambdao
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from odoo import models
 
-import logging
 _logger = logging.getLogger(__name__)
 
 
 class IrActionsReport(models.Model):
 
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
-    def _signable_report(self):
-        """Necessary to get the certificate with py3o reports"""
-        is_py3o_signable = self.report_type == 'py3o' and self.py3o_filetype == 'pdf'
-        return is_py3o_signable or super()._signable_report()
+    def _is_report_type_signable(self):
+        res = super()._is_report_type_signable()
+        return res or (self.report_type == "py3o" and self.py3o_filetype == "pdf")
 
-    def render_py3o(self, res_ids=None, data=None):
-        certificate = self._certificate_get(res_ids)
-        signed_content, ext = self._read_attached_signed_content(res_ids, certificate)
+    def _render_py3o(self, report_ref, res_ids, data=None):
+        report = self._get_report(report_ref)
+        certificate = self._certificate_get(report, res_ids)
+        signed_content, ext = self._read_attached_signed_content(
+            report, res_ids, certificate
+        )
         if signed_content:
-            return signed_content, 'pdf'
-        content, ext = super(IrActionsReport, self).render_py3o(res_ids, data)
+            return signed_content, "pdf"
+        content, ext = super(IrActionsReport, self)._render_py3o(
+            report_ref, res_ids, data
+        )
         if certificate:
             content = self._sign_pdf_and_attach(res_ids, certificate, content)
         return content, ext
--- report_py3o_signer/tests/test_report_py3o_signer.py
+++ report_py3o_signer/tests/test_report_py3o_signer.py
@@ -7,35 +7,36 @@ from odoo.tests.common import TransactionCase
 class TestReportPy3oSignerCase(TransactionCase):
     def setUp(self):
         super(TestReportPy3oSignerCase, self).setUp()
-        self.user = self.env.ref('base.user_admin')
-        self.report = self.env.ref(
-            'report_py3o.res_users_report_py3o'
-        ).with_context(force_report_rendering=True)
-        self.report.py3o_filetype = 'pdf'
+        self.user = self.env.ref("base.user_admin")
+        self.report = self.env.ref("report_py3o.res_users_report_py3o").with_context(
+            force_report_rendering=True
+        )
+        self.report.py3o_filetype = "pdf"
         self.report.attachment_use = True
         self.report.attachment = "'test_' + (object.name or '') + '.pdf'"
 
-        user_model = self.env['ir.model'].search([('model', '=', 'res.users')])
-        self.certificate = self.env.ref('report_qweb_signer.demo_certificate_test')
+        user_model = self.env["ir.model"].search([("model", "=", "res.users")])
+        self.certificate = self.env.ref("report_qweb_signer.demo_certificate_test")
         self.certificate.model_id = user_model
         self.certificate.domain = "[]"
 
 
 class TestReportPy3oSigner(TestReportPy3oSignerCase):
     def test_report_py3o_signer(self):
+        report_ref = self.report.report_name
         user_ids = self.user.ids
-        self.report.render_py3o(user_ids)
+        self.report._render_py3o("py3o_user_info", user_ids)
         # we should find the saved signed content
         signed_content = self.report._attach_signed_read(user_ids, self.certificate)
         self.assertTrue(signed_content)
 
         # Reprint again for taking the PDF from attachment
-        IrAttachment = self.env['ir.attachment']
+        IrAttachment = self.env["ir.attachment"]
         domain = [
-            ('res_id', '=', self.user.id),
-            ('res_model', '=', self.user._name),
+            ("res_id", "=", self.user.id),
+            ("res_model", "=", self.user._name),
         ]
         num_attachments = IrAttachment.search_count(domain)
-        self.report.render_py3o(user_ids)
+        self.report._render_py3o(report_ref, user_ids)
         num_attachments_after = IrAttachment.search_count(domain)
         self.assertEqual(num_attachments, num_attachments_after)
