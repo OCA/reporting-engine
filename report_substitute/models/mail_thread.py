a6e5a21e6ed200b0821a78a5ab8c08a528b2d206
augusto-weiss <awe@adhoc.com.ar>
2025-03-17 14:34:33 -0300

    [FIX] report_substitute: avoid write over the mail template

--- report_substitute/models/mail_thread.py
+++ report_substitute/models/mail_thread.py
@@ -17,17 +17,27 @@ class MailThread(models.AbstractModel):
         **kwargs,
     ):
         template, view = self._get_source_from_ref(source_ref)
-        old_report_template_ids = False
         if template and template.report_template_ids and self.ids:
-            old_report_template_ids = template.report_template_ids
             new_report_template_ids = [
                 report.get_substitution_report(self.ids).id
                 for report in template.report_template_ids
             ]
-            template.update({"report_template_ids": [(6, 0, new_report_template_ids)]})
-        res = super().message_post_with_source(
-            source_ref, render_values, message_type, subtype_xmlid, subtype_id, **kwargs
+            return super(
+                MailThread,
+                self.with_context(default_report_template_ids=new_report_template_ids),
+            ).message_post_with_source(
+                source_ref,
+                render_values,
+                message_type,
+                subtype_xmlid,
+                subtype_id,
+                **kwargs,
+            )
+        return super().message_post_with_source(
+            source_ref,
+            render_values,
+            message_type,
+            subtype_xmlid,
+            subtype_id,
+            **kwargs,
         )
-        if old_report_template_ids:
-            template.report_template_ids = old_report_template_ids
-        return res
