1f30956750c5a1d4e5cf68b71299177cc7283e6c
augusto-weiss <awe@adhoc.com.ar>
2025-10-28 17:34:41 +0000

    [IMP] report_substitute: pre-commit execution

--- report_substitute/__manifest__.py
+++ report_substitute/__manifest__.py
@@ -8,7 +8,7 @@
         """,
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/reporting-engine",
     "depends": ["base", "mail"],
     "data": [
