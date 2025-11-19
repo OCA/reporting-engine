b93803e6395fd9394c571718e06ead6759f11b8f
Aungkokolin1997 <lin@quartile.co>
2025-11-19 08:29:16 +0000

    [MIG] report_text_format_option: Migration to 18.0

--- report_text_format_option/__manifest__.py
+++ report_text_format_option/__manifest__.py
@@ -1,14 +1,15 @@
-# Copyright 2024 Quartile Limited
+# Copyright 2024 Quartile (https://www.quartile.co)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Report Text Format Option",
-    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "author": "Quartile, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/reporting-engine",
     "category": "Reporting",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "LGPL-3",
     "depends": ["base"],
     "data": ["views/ir_actions_report_views.xml"],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
     "demo": ["demo/report_demo.xml"],
     "installable": True,
 }
