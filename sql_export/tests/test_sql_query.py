9a916380ab7f35a04ca1c80ebefe5dff0dcd09db
Mihai Fekete <feketemihai@nexterp.ro>
2026-04-29 19:36:13 +0300

    Update tests

--- sql_export/tests/test_sql_query.py
+++ sql_export/tests/test_sql_query.py
@@ -25,7 +25,7 @@ class TestExportSqlQuery(TransactionCase):
         )
         cls.sql_export_partner = cls.env.ref("sql_export.sql_export_partner")
         cls.sql_export_partner_variables = cls.env.ref(
-            "sql_export.query_properties_definition"
+            "sql_export.sql_export_partner_with_variables"
         )
         cls.sql_export_partner_variables.button_validate_sql_expression()
 
