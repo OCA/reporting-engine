894634cd6d98dbddd3e6f557304ee542a38e76d9
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2026-09-18 15:17:15 +0200

    [MIG] board_eval_context: Migration to 19.0

--- board_eval_context/__manifest__.py
+++ board_eval_context/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Board Eval Context",
     "summary": "Add some keys to board.board eval context",
-    "version": "15.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainer": "gurneyalex",
     "depends": [
